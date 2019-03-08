642b2902bfcaea91f5a95eb27559e0d6dd46a21c
Yoga Hanggara <yohang88@gmail.com>
2019-03-08 15:55:12 +0700

    vue boilerplate from https://github.com/zmts/beauty-vuejs-boilerplate
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

---
+++ frontend/src/.env.js
@@ -0,0 +1,4 @@
+export const API_PORT = '80'
+export const API_URL = `http://localhost/api`
+export const DOMAIN_TITLE = 'Front Store'
+
---
+++ frontend/src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import { initCurrentUserStateMiddleware, checkAccessMiddleware, setPageTitleMiddleware } from './middlewares'
+import { routes } from './routes'
+
+Vue.use(Router)
+
+const router = new Router({
+  linkActiveClass: 'is-active',
+  routes
+})
+
+router.beforeEach(initCurrentUserStateMiddleware)
+router.beforeEach(checkAccessMiddleware)
+router.beforeEach(setPageTitleMiddleware)
+
+export default router
