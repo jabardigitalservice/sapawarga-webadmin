17a74e6a51187f235af47fc6825fc2d7b62e0594
Yoga Hanggara <yohang88@gmail.com>
2019-03-20 16:07:54 +0700

    migration and import data master kab/kota, kelurahan
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- api/migrations/m190319_085306_create_cities_table.php
+++ api/migrations/m190319_085306_create_cities_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 /**
  * Handles the creation of table `cities`.
@@ -13,13 +14,23 @@ class m190319_085306_create_cities_table extends Migration
     public function safeUp()
     {
         $this->createTable('cities', [
-            'id'        => $this->primaryKey(),
-            'parent_id' => $this->integer()->null(),
-            'lft'       => $this->integer()->null(),
-            'rgt'       => $this->integer()->null(),
-            'depth'     => $this->integer()->null(),
-            'name'      => $this->string()->null(),
+            'id'         => $this->primaryKey(),
+            'parent_id'  => $this->integer()->null(),
+            'lft'        => $this->integer()->null(),
+            'rgt'        => $this->integer()->null(),
+            'depth'      => $this->integer()->null(),
+            'name'       => $this->string()->null(),
+            'code1'      => $this->string()->null(),
+            'code2'      => $this->string()->null(),
+            'latitude'   => $this->string()->null(),
+            'longitude'  => $this->string()->null(),
+            'status'     => $this->boolean(),
+            'created_at' => Schema::TYPE_TIMESTAMP . ' DEFAULT CURRENT_TIMESTAMP',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
         ]);
+
+        $sql = file_get_contents(__DIR__ . '/seeder/jawabarat_kabkota_20190320.sql');
+        Yii::$app->db->createCommand($sql)->execute();
     }
 
     /**
