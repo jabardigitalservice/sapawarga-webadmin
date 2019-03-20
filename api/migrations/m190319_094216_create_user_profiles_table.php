41e17eae53e627778d101b13066abfe42f87df37
aldidoanta <aldidkurnia@gmail.com>
2019-03-20 14:10:16 +0700

    Rename user_profile to user_profiles, rename fields

--- api/migrations/m190319_094216_create_user_profiles_table.php
+++ api/migrations/m190319_094216_create_user_profiles_table.php
@@ -5,22 +5,22 @@ use yii\db\Migration;
 /**
  * Class m190319_094216_add_user_profile_table
  */
-class m190319_094216_create_user_profile_table extends Migration
+class m190319_094216_create_user_profiles_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('user_profile', [
+        $this->createTable('user_profiles', [
             'id' => $this->primaryKey(),
-            'nama' => $this->string()->notNull(),
-            'telepon' => $this->string(),
-            'alamat' => $this->string(),
+            'name' => $this->string()->notNull(),
+            'phone' => $this->string(),
+            'address' => $this->string(),
             'rw' => $this->string()->notNull(),
             'kel_id' => $this->string()->notNull(),
             'kec_id' => $this->string()->notNull(),
-            'kab_id' => $this->string()->notNull(),
+            'kokab_id' => $this->string()->notNull(),
         ]);
     }
 
@@ -29,7 +29,6 @@ class m190319_094216_create_user_profile_table extends Migration
      */
     public function safeDown()
     {
-
         $this->dropTable('user_profile');
     }
 }
