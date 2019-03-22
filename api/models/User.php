dafddc046e5ae3cb972c4026a0d73834e4f24c89
aldidoanta <aldidkurnia@gmail.com>
2019-03-22 13:35:33 +0700

    Remove 'required' rule for rw, kel_id, kec_id, and kabkota_id

--- api/models/User.php
+++ api/models/User.php
@@ -425,8 +425,7 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
             ['permissions', 'validatePermissions'],
             [['access_token', 'permissions'], 'safe'],
             ['phone', 'trim'],
-            [['name', 'phone', 'address'], 'default'],
-            [['rw', 'kel_id', 'kec_id', 'kabkota_id'], 'required'],
+            [['name', 'phone', 'address', 'rw', 'kel_id', 'kec_id', 'kabkota_id'], 'default'],
         ];
     }
 
