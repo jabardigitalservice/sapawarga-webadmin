06e46a68d14c12f211613dcb5a4ea2628b952dc3
Aldi Rohman <aldirohman@live.com>
2019-03-26 16:34:57 +0700

    addded direct app external fb, ig, twitter, & validator sosmed

--- mobile/src/app/pages/view-profile/view-profile.page.ts
+++ mobile/src/app/pages/view-profile/view-profile.page.ts
@@ -3,10 +3,16 @@ import { ProfileService } from 'src/app/services/profile.service';
 import {
   LoadingController,
   PopoverController,
-  ToastController
+  ToastController,
+  Platform
 } from '@ionic/angular';
 import { Profile } from '../../interfaces/profile';
 import { MenuNavbarComponent } from '../../components/menu-navbar/menu-navbar.component';
+import { AppAvailability } from '@ionic-native/app-availability/ngx';
+import {
+  InAppBrowser,
+  InAppBrowserObject
+} from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-view-profile',
@@ -20,7 +26,10 @@ export class ViewProfilePage implements OnInit {
     public loadingCtrl: LoadingController,
     private profileService: ProfileService,
     public popoverCtrl: PopoverController,
-    public toastCtrl: ToastController
+    public toastCtrl: ToastController,
+    private appAvailability: AppAvailability,
+    private platform: Platform,
+    private inAppBrowser: InAppBrowser
   ) {}
 
   ngOnInit() {
@@ -29,6 +38,78 @@ export class ViewProfilePage implements OnInit {
 
   ionViewDidEnter() {
     this.getDataProfile(null);
+  }
+
+  // pass in the app name and the name of the user/page
+  goToSosialMedia(app: string, name?: string, fbUrl?: string) {
+    if (name) {
+      switch (app) {
+        case 'facebook':
+          this.launchApp(
+            'fb://',
+            'com.facebook.katana',
+            'fb://facewebmodal/f?href=' + fbUrl,
+            // url fb
+            fbUrl
+          );
+          break;
+        case 'instagram':
+          this.launchApp(
+            'instagram://',
+            'com.instagram.android',
+            'instagram://user?username=' + name,
+            'https://www.instagram.com/' + name
+          );
+          break;
+        case 'twitter':
+          this.launchApp(
+            'twitter://',
+            'com.twitter.android',
+            'twitter://user?screen_name=' + name,
+            'https://twitter.com/' + name
+          );
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  private launchApp(
+    iosApp: string,
+    androidApp: string,
+    appUrl: string,
+    webUrl: string
+  ) {
+    let app: string;
+    // check if the platform is ios or android, else open the web url
+    if (this.platform.is('ios')) {
+      app = iosApp;
+    } else if (this.platform.is('android')) {
+      app = androidApp;
+    } else {
+      const browser: InAppBrowserObject = this.inAppBrowser.create(
+        webUrl,
+        '_system'
+      );
+      return;
+    }
+    this.appAvailability.check(app).then(
+      () => {
+        // success callback, the app exists and we can open it
+        const browser: InAppBrowserObject = this.inAppBrowser.create(
+          appUrl,
+          '_system'
+        );
+      },
+      () => {
+        // error callback, the app does not exist, open regular web url instead
+        const browser: InAppBrowserObject = this.inAppBrowser.create(
+          webUrl,
+          '_system'
+        );
+      }
+    );
   }
 
   async getDataProfile(event) {
@@ -43,8 +124,8 @@ export class ViewProfilePage implements OnInit {
         this.dataProfile = res['data'];
         loader.dismiss();
         if (
-          !this.dataProfile.twitter &&
-          !this.dataProfile.facebook &&
+          !this.dataProfile.twitter ||
+          !this.dataProfile.facebook ||
           !this.dataProfile.instagram
         ) {
           this.showToast('Mohon lengkapi akun sosial media anda');
@@ -56,6 +137,10 @@ export class ViewProfilePage implements OnInit {
       }
     );
   }
+
+  // goToSosialMedia(value: string) {
+  //   console.log(value);
+  // }
 
   doRefresh(event) {
     this.getDataProfile('loading');
@@ -73,6 +158,7 @@ export class ViewProfilePage implements OnInit {
         dataUser: ev
       },
       event: ev,
+      cssClass: 'popover_class',
       animated: true,
       showBackdrop: true,
       translucent: true
@@ -81,10 +167,6 @@ export class ViewProfilePage implements OnInit {
     popover.onDidDismiss();
 
     return await popover.present();
-  }
-
-  goToSosialMedia(value: string) {
-    console.log(value);
   }
 
   async showToast(msg: string) {
