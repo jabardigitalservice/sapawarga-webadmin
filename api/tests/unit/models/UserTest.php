43d61e8bf3bdf3c7ab13f67b7a789065ce5e5ca2
aldidoanta <aldidkurnia@gmail.com>
2019-03-26 10:12:56 +0700

    Simplify codebase for User unit test

--- api/tests/unit/models/UserTest.php
+++ api/tests/unit/models/UserTest.php
@@ -10,6 +10,10 @@ class UserTest extends \Codeception\Test\Unit
 
     use Specify;
 
+    private $username = 'username';
+    private $email = 'email';
+    private $password = 'password';
+
     public function testValidateUserFields()
     {
         // Validation requires method to be set
@@ -30,8 +34,8 @@ class UserTest extends \Codeception\Test\Unit
                 $this->assertFalse($user->validate());
 
                 // Verify that the username and password properties are required
-                $this->assertTrue($user->hasErrors('username'));
-                $this->assertTrue($user->hasErrors('email'));
+                $this->assertTrue($user->hasErrors($this->username));
+                $this->assertTrue($user->hasErrors($this->email));
 
                 // Set temporary values for username and email
                 $user->username = 'username';
@@ -51,23 +55,16 @@ class UserTest extends \Codeception\Test\Unit
                 // Set username length as 2
                 $user->username = 'ab';
                 // Verify that validation must be failed
-                $this->assertFalse($user->validate());
-                // Verify that username validation contains error message
-                $this->assertTrue($user->hasErrors('username'));
+                $this->assertFalse($user->validate($this->username));
 
                 // Set username length as 5
                 $user->username = 'abcde';
                 // Verify that validation must be failed
-                $this->assertFalse($user->validate());
-                // Verify that username validation does not contain error message
-                $this->assertFalse($user->hasErrors('username'));
+                $this->assertTrue($user->validate($this->username));
 
                 // Set username length as 15
                 $user->username = 'abcdefghijklmno';
-                // Verify that validation must be failed
-                $this->assertFalse($user->validate());
-                // Verify that username validation contains error message
-                $this->assertTrue($user->hasErrors('username'));
+                $this->assertFalse($user->validate($this->username));
             }
         );
 
@@ -75,20 +72,16 @@ class UserTest extends \Codeception\Test\Unit
             $user = new User();
 
             $user->username = 'user-name';
-            $this->assertFalse($user->validate());
-            $this->assertTrue($user->hasErrors('username'));
+            $this->assertFalse($user->validate($this->username));
 
             $user->username = 'user name';
-            $this->assertFalse($user->validate());
-            $this->assertTrue($user->hasErrors('username'));
+            $this->assertFalse($user->validate($this->username));
 
             $user->username = 'userName';
-            $this->assertFalse($user->validate());
-            $this->assertTrue($user->hasErrors('username'));
+            $this->assertFalse($user->validate($this->username));
 
             $user->username = 'my.user_name01';
-            $this->assertFalse($user->validate());
-            $this->assertFalse($user->hasErrors('username'));
+            $this->assertTrue($user->validate($this->username));
         });
 
         $this->specify('username is unique', function () {
@@ -100,54 +93,32 @@ class UserTest extends \Codeception\Test\Unit
             $user = new User();
 
             $user->email = 'test@example.com';
-            $this->assertFalse($user->validate());
-            $this->assertFalse($user->hasErrors('email'));
+            $this->assertTrue($user->validate($this->email));
 
             $user->email = 'user@4koz0713e0irdqhwlq9vo4osgh1dov14ywb6sxdresfu77zfeldz5ftq0w7o6dmerq2stcrqiwvg8ddzh2sjeot4vrqox9mc7px3k6uren0aqgqo3n63lbcn3mqb68a5fncp9xb51jhgqkmmuay0z6sfhrzgvtbeblvt565et7hqr4lqndfxllvprp02u3juljmdltae9vc2vrewrvvfiqmtnr3mdi2w9dghyzhsf5i585pxfjprk9.com';
-            $this->assertFalse($user->validate());
-            $this->assertTrue($user->hasErrors('email'));
+            $this->assertFalse($user->validate($this->email));
         });
 
         $this->specify('email must be valid', function () {
             $user = new User();
 
             $user->email = 'test.example.com';
-            $this->assertFalse($user->validate());
-            $this->assertTrue($user->hasErrors('email'));
+            $this->assertFalse($user->validate($this->email));
 
             $user->email = 'test user@example.com';
-            $this->assertFalse($user->validate());
-            $this->assertTrue($user->hasErrors('email'));
+            $this->assertFalse($user->validate($this->email));
 
             $user->email = 'test@example.com';
-            $this->assertFalse($user->validate());
-            $this->assertFalse($user->hasErrors('email'));
+            $this->assertTrue($user->validate($this->email));
 
             $user->email = 'test-user.name01@example.com';
-            $this->assertFalse($user->validate());
-            $this->assertFalse($user->hasErrors('email'));
+            $this->assertTrue($user->validate($this->email));
         });
 
         $this->specify('email is unique', function () {
         });
 
-
-        // Tests for 'email' field
         $this->specify('password is required on POST method', function () {
-        });
-
-        $this->specify('password must be a minimum of 5 characters', function () {
-            $user = new User();
-            $user->username = 'username';
-            $user->email = 'test@example.com';
-
-            // $user->password = 1234;
-            // $this->assertFalse($user->validate());
-            // $this->assertTrue($user->hasErrors('password'));
-
-            $user->password = '12345';
-            $this->assertTrue($user->validate());
-            $this->assertFalse($user->hasErrors('password'));
         });
 
         $this->specify('status must be Active or Disabled', function () {
