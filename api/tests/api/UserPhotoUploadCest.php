08a21c108fd284e3fb75d63b87cab10d7818a54f
Yoga Hanggara <yohang88@gmail.com>
2019-03-27 09:57:31 +0700

    [API] user photo upload functional API test (negative case)
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- api/tests/api/UserPhotoUploadCest.php
+++ api/tests/api/UserPhotoUploadCest.php
@@ -38,4 +38,67 @@ class UserPhotoUploadCest
             'status'  => 200,
         ]);
     }
+
+    public function postPhotoUploadNoFileTest(ApiTester $I)
+    {
+        $I->deleteHeader('Content-Type');
+
+        $I->sendPOST('/v1/user/photo');
+
+        $I->canSeeResponseCodeIs(422);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 422,
+        ]);
+    }
+
+    public function postPhotoUploadInvalidFileTypeTest(ApiTester $I)
+    {
+        $I->deleteHeader('Content-Type');
+
+        $filePath = __DIR__ . '/../data/example.txt';
+
+        $I->sendPOST('/v1/user/photo', [], ['image' => [
+                'name'     => 'example.txt',
+                'type'     => 'text/plain',
+                'error'    => UPLOAD_ERR_OK,
+                'size'     => filesize($filePath),
+                'tmp_name' => $filePath,
+            ],
+        ]);
+
+        $I->canSeeResponseCodeIs(422);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 422,
+        ]);
+    }
+
+    public function postPhotoUploadInvalidHeaderMimeTest(ApiTester $I)
+    {
+        $I->deleteHeader('Content-Type');
+
+        $filePath = __DIR__ . '/../data/example.txt';
+
+        $I->sendPOST('/v1/user/photo', [], ['image' => [
+            'name'     => 'example.jpg',
+            'type'     => 'image/jpeg',
+            'error'    => UPLOAD_ERR_OK,
+            'size'     => filesize($filePath),
+            'tmp_name' => $filePath,
+        ],
+        ]);
+
+        $I->canSeeResponseCodeIs(422);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 422,
+        ]);
+    }
 }
