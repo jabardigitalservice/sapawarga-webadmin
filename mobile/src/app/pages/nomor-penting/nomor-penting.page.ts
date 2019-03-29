ae06187830477283823d322a380158658eb3bb71
Ma'ruf <maruf.harsono@gmail.com>
2019-03-29 13:11:53 +0700

    mobile nomorPenting Styling nomorPenting page

---
+++ mobile/src/app/pages/nomor-penting/nomor-penting.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nomor-penting',
+  templateUrl: './nomor-penting.page.html',
+  styleUrls: ['./nomor-penting.page.scss']
+})
+export class NomorPentingPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
