cc5ec8f54a365b2a8fb6a282fcae8f01e49b01bc
Yoga Hanggara <yohang88@gmail.com>
2019-04-02 11:29:48 +0700

    [API] User Seeder
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

---
+++ api/commands/SeederController.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace app\commands;
+
+use Yii;
+use yii\console\Controller;
+
+class SeederController extends Controller
+{
+    public function actionUser()
+    {
+        Yii::$app->db->createCommand("TRUNCATE auth_assignment")->execute();
+        Yii::$app->db->createCommand("TRUNCATE user")->execute();
+
+        Yii::$app->db->createCommand()->batchInsert('user', [
+            'id',
+            'username',
+            'auth_key',
+            'access_token_expired_at',
+            'password_hash',
+            'password_reset_token',
+            'email',
+            'unconfirmed_email',
+            'confirmed_at',
+            'registration_ip',
+            'last_login_at',
+            'last_login_ip',
+            'blocked_at',
+            'status',
+            'role',
+            'created_at',
+            'updated_at'
+        ], [
+            [
+                1,
+                'admin',
+                'dVN8fzR_KzJ_lBrymfXI6qyH2QzyXYUU',
+                '2017-06-04 00:13:29',
+                '$2y$13$9Gouh1ZbewVEh4bQIGsifOs8/RWW/7RIs0CAGNd7tapXFm9.WxiXS',
+                null,
+                'admin@demo.com',
+                'admin@demo.com',
+                time(),
+                '127.0.0.1',
+                time(),
+                '127.0.0.1',
+                null,
+                10,
+                99,
+                time(),
+                time()
+            ],
+            [
+                2,
+                'staff',
+                'Xm-zZRREtAIKsFlINVRLSw3U7llbx_5a',
+                '2017-05-30 20:30:31',
+                '$2y$13$TKh5pEy0RFTmkC9Kjvb9A.WR/I1QVzYHdfYDw0m7MnHnN0bsv96Jq',
+                null,
+                'staff@demo.com',
+                'staff@demo.com',
+                time(),
+                '127.0.0.1',
+                time(),
+                '127.0.0.1',
+                null,
+                10,
+                50,
+                time(),
+                time()
+            ],
+        ])->execute();
+
+        Yii::$app->db->createCommand()->batchInsert('user', [
+            'id',
+            'username',
+            'auth_key',
+            'access_token_expired_at',
+            'password_hash',
+            'password_reset_token',
+            'email',
+            'unconfirmed_email',
+            'confirmed_at',
+            'registration_ip',
+            'last_login_at',
+            'last_login_ip',
+            'blocked_at',
+            'status',
+            'role',
+            'created_at',
+            'updated_at',
+            'name',
+            'phone',
+            'address',
+            'rw',
+            'kel_id',
+            'kec_id',
+            'kabkota_id',
+        ], [
+            [
+                3,
+                'user',
+                'rNXSqIas_43RdpG0e5_7d1W06iK8pXJ8',
+                '2017-06-04 00:13:02',
+                '$2y$13$nd/F3g6jjIa1/Sk6JZxZ5uVq0OpsbOmW1OdnbDG6BpFqgkFbQotjm',
+                null,
+                'user@demo.com',
+                'user@demo.com',
+                time(),
+                '127.0.0.1',
+                time(),
+                '127.0.0.1',
+                null,
+                10,
+                10,
+                time(),
+                time(),
+                'User Kota Bandung Satu',
+                '0857123456',
+                'Jl. Alamat Kota Bandung Satu',
+                2,
+                6178,
+                446,
+                22,
+            ],
+            [
+                4,
+                'user.bandung',
+                'rNXSqIas_43RdpG0e5_7d1W06iK8pXJ8',
+                '2017-06-04 00:13:02',
+                '$2y$13$nd/F3g6jjIa1/Sk6JZxZ5uVq0OpsbOmW1OdnbDG6BpFqgkFbQotjm',
+                null,
+                'user.bandung@demo.com',
+                'user.bandung@demo.com',
+                time(),
+                '127.0.0.1',
+                time(),
+                '127.0.0.1',
+                null,
+                10,
+                10,
+                time(),
+                time(),
+                'User Kota Bandung Dua',
+                '0857123456',
+                'Jl. Alamat Kota Bandung Dua',
+                2,
+                6178,
+                446,
+                22,
+            ],
+            [
+                5,
+                'user.bekasi',
+                'rNXSqIas_43RdpG0e5_7d1W06iK8pXJ8',
+                '2017-06-04 00:13:02',
+                '$2y$13$nd/F3g6jjIa1/Sk6JZxZ5uVq0OpsbOmW1OdnbDG6BpFqgkFbQotjm',
+                null,
+                'user.bekasi@demo.com',
+                'user.bekasi@demo.com',
+                time(),
+                '127.0.0.1',
+                time(),
+                '127.0.0.1',
+                null,
+                10,
+                10,
+                time(),
+                time(),
+                'User Kota Bekasi',
+                '0857123456',
+                'Jl. Alamat Kota Bekasi',
+                9,
+                6228,
+                452,
+                23,
+            ],
+            [
+                6,
+                'user.tasikmalaya',
+                'rNXSqIas_43RdpG0e5_7d1W06iK8pXJ8',
+                '2017-06-04 00:13:02',
+                '$2y$13$nd/F3g6jjIa1/Sk6JZxZ5uVq0OpsbOmW1OdnbDG6BpFqgkFbQotjm',
+                null,
+                'user.tasikmalaya@demo.com',
+                'user.tasikmalaya@demo.com',
+                time(),
+                '127.0.0.1',
+                time(),
+                '127.0.0.1',
+                null,
+                10,
+                10,
+                time(),
+                time(),
+                'User Kota Tasikmalaya',
+                '0857123456',
+                'Jl. Alamat Kota Tasikmalaya',
+                11,
+                6341,
+                466,
+                26,
+            ],
+        ])->execute();
+
+        $auth = Yii::$app->authManager;
+
+        $admin = $auth->getRole('admin');
+        $auth->assign($admin, 1);
+
+        $staff = $auth->getRole('staff');
+        $auth->assign($staff, 2);
+
+        $user = $auth->getRole('user');
+
+        $auth->assign($user, 3);
+        $auth->assign($user, 4);
+        $auth->assign($user, 5);
+        $auth->assign($user, 6);
+    }
+}
