396c9af8f9d8209800333f8a43c3b85c21c8b715
aldidoanta <aldidkurnia@gmail.com>
2019-04-04 11:56:14 +0700

    Refactor test cases for User and Staff login

--- api/tests/api/StaffCest.php
+++ api/tests/api/StaffCest.php
@@ -4,6 +4,34 @@ class StaffCest
 {
     private $endpoint = '/v1/staff/login';
 
+    protected function login(ApiTester $I)
+    {
+        $I->sendPOST($this->endpoint, [
+            'LoginForm' => [
+                'username' => 'admin',
+                'password' => '123456',
+            ]
+        ]);
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status' => 200,
+        ]);
+
+        $I->seeResponseMatchesJsonType([
+            'id' => 'integer',
+            'access_token' => 'string',
+        ], '$.data');
+
+        $token = $I->grabDataFromResponseByJsonPath('$..data.access_token');
+        $token = $token[0];
+
+        $I->amBearerAuthenticated($token);
+    }
+
     public function staffLoginInvalidFields(ApiTester $I)
     {
         $I->sendPOST($this->endpoint);
@@ -63,31 +91,10 @@ class StaffCest
         ]);
     }
 
+    /**
+     * @before login
+     */
     public function staffLogin(ApiTester $I)
     {
-        $I->sendPOST($this->endpoint, [
-            'LoginForm' => [
-                'username' => 'admin',
-                'password' => '123456',
-            ]
-        ]);
-
-        $I->seeResponseCodeIs(200);
-        $I->seeResponseIsJson();
-
-        $I->seeResponseContainsJson([
-            'success' => true,
-            'status' => 200,
-        ]);
-
-        $I->seeResponseMatchesJsonType([
-            'id' => 'integer',
-            'access_token' => 'string',
-        ], '$.data');
-
-        $token = $I->grabDataFromResponseByJsonPath('$..data.access_token');
-        $token = $token[0];
-
-        $I->amBearerAuthenticated($token);
     }
 }
