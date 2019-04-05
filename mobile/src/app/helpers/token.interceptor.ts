efbeaa9c83f28d4a23a795bf6a7d0d306d6f4a76
Aldi Rohman <aldirohman@live.com>
2019-04-05 16:50:19 +0700

    added clear local storage when status 401

--- mobile/src/app/helpers/token.interceptor.ts
+++ mobile/src/app/helpers/token.interceptor.ts
@@ -10,28 +10,19 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { Storage } from '@ionic/storage';
 import { ToastController } from '@ionic/angular';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(
-    private storage: Storage,
-    private router: Router,
-    private toastCtrl: ToastController
-  ) {}
+  constructor(private router: Router, private toastCtrl: ToastController) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    // const aaaa = this.storage.get('auth-token');
     const token = localStorage.getItem('auth-token')
       ? localStorage.getItem('auth-token')
       : '';
-    // this.storage.get('auth-token').then(val => {
-    //   console.log('Your age is', val);
-    // });
     if (token) {
       request = request.clone({
         setHeaders: {
@@ -55,7 +46,6 @@ export class TokenInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       map((event: HttpEvent<any>) => {
         if (event instanceof HttpResponse) {
-          // console.log('event--->>>', event);
         }
         return event;
       }),
@@ -67,8 +57,9 @@ export class TokenInterceptor implements HttpInterceptor {
             this.router.navigate(['login']);
           }
           this.showToast(error.error.data.name, error.error.data.message);
-          // console.log(error.error.data.message);
           this.router.navigate(['login']);
+          // clear locastorage
+          localStorage.clear();
         }
         return throwError(error);
       })
