e25198ae1fb4726e7eeb84903ce9fe61f37c0d49
aldidoanta <aldidkurnia@gmail.com>
2019-04-08 09:09:45 +0700

    [API] [User] Fix issue when permissions are not assigned to user

--- api/models/User.php
+++ api/models/User.php
@@ -425,7 +425,7 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
     public function scenarios()
     {
         $scenarios = parent::scenarios();
-        $scenarios[self::SCENARIO_REGISTER] = ['username', 'email', 'password', 'role', 'kabkota_id', 'kec_id', 'kel_id', 'rw'];
+        $scenarios[self::SCENARIO_REGISTER] = ['username', 'email', 'password', 'role', 'kabkota_id', 'kec_id', 'kel_id', 'rw', 'permissions'];
         return $scenarios;
     }
 
