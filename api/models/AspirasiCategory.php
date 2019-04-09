9a1001695f023dac2d1614e998215419294d8e73
Yoga Hanggara <yohang88@gmail.com>
2019-04-09 14:20:40 +0700

    [API] [Aspirasi] TDD API Test Aspirasi Category
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

---
+++ api/models/AspirasiCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "aspirasi_categories".
+ *
+ * @property int $id
+ */
+class AspirasiCategory extends \yii\db\ActiveRecord
+{
+    //
+}
