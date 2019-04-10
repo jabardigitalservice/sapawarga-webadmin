3ce3a2ee562bc97a5a6d3a3d1285f825ed7a71ca
Ma'ruf <maruf.harsono@gmail.com>
2019-04-10 11:33:13 +0700

    Admin - tambahUser layout

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from '@/layout';
 
 /* Router Modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
+import componentsRouter from './modules/components';
+import chartsRouter from './modules/charts';
+import tableRouter from './modules/table';
+import nestedRouter from './modules/nested';
 
 /** note: sub-menu only appear when children.length>=1
  *  detail see  https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
@@ -79,10 +79,45 @@ export const constantRoutes = [
         path: 'dashboard',
         component: () => import('@/views/dashboard/index'),
         name: 'Dashboard',
-        meta: { title: 'dashboard', icon: 'dashboard', noCache: true, affix: true }
+        meta: {
+          title: 'dashboard',
+          icon: 'dashboard',
+          noCache: true,
+          affix: true
+        }
       }
     ]
   },
+
+  {
+    path: '/user',
+    component: Layout,
+    redirect: '/user/user-list',
+    name: 'user',
+    meta: {
+      title: 'User',
+      icon: 'user'
+    },
+    children: [
+      {
+        path: 'user-list',
+        component: () => import('@/views/user/list'),
+        name: 'userList',
+        meta: {
+          title: 'User List'
+        }
+      },
+      {
+        path: 'user-add',
+        component: () => import('@/views/user/add'),
+        name: 'userAdd',
+        meta: {
+          title: 'User Add'
+        }
+      }
+    ]
+  },
+
   {
     path: '/documentation',
     component: Layout,
@@ -91,7 +126,11 @@ export const constantRoutes = [
         path: 'index',
         component: () => import('@/views/documentation/index'),
         name: 'Documentation',
-        meta: { title: 'documentation', icon: 'documentation', affix: true }
+        meta: {
+          title: 'documentation',
+          icon: 'documentation',
+          affix: true
+        }
       }
     ]
   },
@@ -108,12 +147,12 @@ export const constantRoutes = [
       }
     ]
   }
-]
+];
 
 /**
  * asyncRoutes
  * the routes that need to be dynamically loaded based on user roles
-*/
+ */
 export const asyncRoutes = [
   {
     path: '/permission',
@@ -384,20 +423,21 @@ export const asyncRoutes = [
   },
 
   { path: '*', redirect: '/404', hidden: true }
-]
+];
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
---
+++ backend/src/views/user/add.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app-container">
+    <p class="warn-content">Tambah Pengguna</p>
+    <el-row :gutter="20">
+      <el-col :span="5">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="13">
+        <div class="grid-content bg-purple-light"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<style lang="scss" scoped>
+p {
+  color: #42b983;
+  font-weight: 600;
+}
+.app-container {
+  padding: 5px 20px;
+}
+.el-row {
+  margin-left: 20px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
