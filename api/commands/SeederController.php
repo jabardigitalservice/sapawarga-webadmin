d4f5eb5a9c33cdc80c59a0114d707076147a9ef1
Yoga Hanggara <yohang88@gmail.com>
2019-04-11 14:18:30 +0700

    [API] [PhoneBook] Categories seeder, using URL random faker
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- api/commands/SeederController.php
+++ api/commands/SeederController.php
@@ -18,6 +18,9 @@ class SeederController extends Controller
         echo 'Seeding Users...' . PHP_EOL;
         $this->actionUser();
 
+        echo 'Seeding Phonebooks Categories...' . PHP_EOL;
+        $this->actionPhoneBookCategories();
+
         echo 'Seeding Phonebooks...' . PHP_EOL;
         $this->actionPhoneBook();
     }
@@ -435,7 +438,44 @@ class SeederController extends Controller
         $auth->assign($user, 12);
     }
 
-    public function actionPhoneBook()
+    public function actionPhoneBookCategories()
+    {
+        Yii::$app->db->createCommand('TRUNCATE phonebooks_categories')->execute();
+
+        Yii::$app->db->createCommand()->batchInsert('phonebooks_categories', [
+            'name',
+            'status',
+            'created_at',
+            'updated_at',
+        ], [
+            [
+                'Kesehatan',
+                10,
+                time(),
+                time(),
+            ],
+            [
+                'Ekonomi',
+                10,
+                time(),
+                time(),
+            ],
+            [
+                'Keamanan',
+                10,
+                time(),
+                time(),
+            ],
+            [
+                'Transportasi',
+                10,
+                time(),
+                time(),
+            ],
+        ])->execute();
+    }
+
+        public function actionPhoneBook()
     {
         Yii::$app->db->createCommand('TRUNCATE phonebooks')->execute();
 
@@ -465,7 +505,8 @@ class SeederController extends Controller
             'seq' => 1,
             'latitude' => $faker->latitude('-6.148534', '-7.484602'),
             'longitude' => $faker->longitude('106.022438', '108.680921'),
-            'cover_image_path' => 'https://dummyimage.com/wsxga',
+            'cover_image_path' => $faker->optional()->imageUrl(800, 480, 'city'),
+            'category_id' => $faker->randomElement([1, 2, 3, 4]),
             'created_at' => time(),
             'updated_at' => time(),
         ])->rowQuantity(50);
