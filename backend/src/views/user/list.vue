41761b924a0a54362846f24320fcac45051e7859
Yoga Hanggara <yohang88@gmail.com>
2019-04-16 11:26:27 +0700

    [Admin] User List layout
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- backend/src/views/user/list.vue
+++ backend/src/views/user/list.vue
@@ -1,56 +1,64 @@
 <template>
   <div class="app-container">
-    <el-table v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%">
-      <el-table-column align="center" label="ID" width="80">
-        <template slot-scope="scope">
-          <span>{{ scope.row.id }}</span>
-        </template>
-      </el-table-column>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        Components Statistik Pengguna
+      </el-col>
 
-      <el-table-column label="Name">
-        <template slot-scope="scope">
-          <span>{{ scope.row.name }}</span>
-        </template>
-      </el-table-column>
+      <el-col :span="18">
+        <el-table v-loading="listLoading" :data="list" border stripe fit highlight-current-row style="width: 100%">
+          <el-table-column align="center" label="ID" width="80">
+            <template slot-scope="scope">
+              <span>{{ scope.row.id }}</span>
+            </template>
+          </el-table-column>
 
-      <el-table-column label="Kedudukan">
-        <template slot-scope="scope">
-          <span>{{ scope.row.address }}</span>
-        </template>
-      </el-table-column>
+          <el-table-column label="Name">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
 
-      <el-table-column label="Telp">
-        <template slot-scope="scope">
-          <span>{{ scope.row.phone }}</span>
-        </template>
-      </el-table-column>
+          <el-table-column label="Kedudukan">
+            <template slot-scope="scope">
+              <span>{{ scope.row.address }}</span>
+            </template>
+          </el-table-column>
 
-      <el-table-column label="Role">
-        <template slot-scope="scope">
-          <span>{{ scope.row.role_label }}</span>
-        </template>
-      </el-table-column>
+          <el-table-column label="Telp">
+            <template slot-scope="scope">
+              <span>{{ scope.row.phone }}</span>
+            </template>
+          </el-table-column>
 
-      <el-table-column class-name="status-col" label="Status" width="200">
-        <template slot-scope="{row}">
-          <el-tag :type="row.status | statusFilter">
-            {{ row.status_label }}
-          </el-tag>
-        </template>
-      </el-table-column>
+          <el-table-column label="Role">
+            <template slot-scope="scope">
+              <span>{{ scope.row.role_label }}</span>
+            </template>
+          </el-table-column>
 
-      <el-table-column align="center" label="Actions" width="120">
-        <template slot-scope="scope">
-          <router-link :to="'/user/edit/'+scope.row.id">
-            <el-button type="primary" size="small" icon="el-icon-edit">
-              Edit
-            </el-button>
-          </router-link>
-        </template>
-      </el-table-column>
-    </el-table>
+          <el-table-column class-name="status-col" label="Status" width="150px">
+            <template slot-scope="{row}">
+              <el-tag :type="row.status | statusFilter">
+                {{ row.status_label }}
+              </el-tag>
+            </template>
+          </el-table-column>
 
-    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+          <el-table-column align="center" label="Actions" width="120">
+            <template slot-scope="scope">
+              <router-link :to="'/user/edit/'+scope.row.id">
+                <el-button type="primary" size="small" icon="el-icon-edit">
+                  Edit
+                </el-button>
+              </router-link>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
