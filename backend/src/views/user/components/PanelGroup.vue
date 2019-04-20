cc2abed9f0cb448e4a9dc7eaf478803f02ef462c
Ganjar Setia <ganjar_setia@ymail.com>
2019-04-20 18:58:37 +0700

    Add layout total user for province, kota, kec, kel, rw, rt

--- backend/src/views/user/components/PanelGroup.vue
+++ backend/src/views/user/components/PanelGroup.vue
@@ -1,57 +1,113 @@
 <template>
   <el-row :gutter="40" class="panel-group">
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel">
-        <div class="card-panel-icon-wrapper icon-people">
-          <svg-icon icon-class="peoples" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            Total Pengguna
-          </div>
-          <count-to :start-val="0" :end-val="totalAllUser" :duration="2600" class="card-panel-num" />
-        </div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>Total user per:</span>
       </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel">
-        <div class="card-panel-icon-wrapper icon-message">
-          <svg-icon icon-class="message" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            Messages
+      <div style="margin-bottom:50px;">
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class="card-panel">
+            <div class="card-panel-icon-wrapper icon-people-all">
+              <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">
+                Semua
+              </div>
+              <count-to :start-val="0" :end-val="totalAllUser" :duration="duration" class="card-panel-num" />
+            </div>
           </div>
-          <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel">
-        <div class="card-panel-icon-wrapper icon-money">
-          <svg-icon icon-class="money" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            Purchases
+        </el-col>
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class="card-panel">
+            <div class="card-panel-icon-wrapper icon-people-province">
+              <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">
+                Provinsi
+              </div>
+              <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
+            </div>
           </div>
-          <count-to :start-val="0" :end-val="9280" :duration="3200" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel">
-        <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="shopping" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            Shoppings
+        </el-col>
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class="card-panel">
+            <div class="card-panel-icon-wrapper icon-people-kota">
+              <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">
+                Kota/Kab
+              </div>
+              <count-to :start-val="0" :end-val="9280" :duration="3200" class="card-panel-num" />
+            </div>
           </div>
-          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
-        </div>
+        </el-col>
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class="card-panel">
+            <div class="card-panel-icon-wrapper icon-people-kec">
+              <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">
+                Kecamatan
+              </div>
+              <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
+            </div>
+          </div>
+        </el-col>
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class="card-panel">
+            <div class="card-panel-icon-wrapper icon-people-kel">
+              <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">
+                Kelurahan
+              </div>
+              <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
+            </div>
+          </div>
+        </el-col>
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class="card-panel">
+            <div class="card-panel-icon-wrapper icon-people-rw">
+              <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">
+                RW
+              </div>
+              <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
+            </div>
+          </div>
+        </el-col>
+        <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+          <div class="card-panel">
+            <div class="card-panel-icon-wrapper icon-people-rt">
+              <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">
+                RT
+              </div>
+              <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
+            </div>
+          </div>
+        </el-col>
+        <!-- <el-col :xs="12" :sm="12" :lg="6">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>Total Pengguna</span>
+            </div>
+            <div class="component-item">
+              <count-to :start-val="0" :end-val="totalAllUser" :duration="duration" class="" />
+            </div>
+          </el-card>
+        </el-col> -->
       </div>
-    </el-col>
+    </el-card>
   </el-row>
 </template>
 
@@ -60,13 +116,17 @@ import CountTo from 'vue-count-to'
 
 export default {
   name: 'PanelGroupUser',
-  props: {
-    totalAllUser: {
-      type: Number,
-    }
-  },
   components: {
     CountTo
+  },
+  props: {
+    totalAllUser: {
+      type: Number
+    },
+    duration: {
+      type: Number,
+      default: 2600
+    }
   }
 }
 </script>
@@ -91,8 +151,26 @@ export default {
       .card-panel-icon-wrapper {
         color: #fff;
       }
-      .icon-people {
+      .icon-people-all {
          background: #40c9c6;
+      }
+      .icon-people-province {
+         background: #4048c9;
+      }
+      .icon-people-kota {
+         background: #c94096;
+      }
+      .icon-people-kec {
+         background: #6cc940;
+      }
+      .icon-people-kel {
+         background: #c8da49;
+      }
+      .icon-people-rw {
+         background: #dabd49;
+      }
+      .icon-people-rt {
+         background: #76888e;
       }
       .icon-message {
         background: #36a3f7;
@@ -104,8 +182,26 @@ export default {
         background: #34bfa3
       }
     }
-    .icon-people {
+    .icon-people-all {
       color: #40c9c6;
+    }
+    .icon-people-province {
+      color: #4048c9;
+    }
+    .icon-people-kota {
+      color: #c94096;
+    }
+    .icon-people-kec {
+      color: #6cc940;
+    }
+    .icon-people-kel {
+      color: #c8da49;
+    }
+    .icon-people-rw {
+      color: #dabd49;
+    }
+    .icon-people-rt {
+      color: #76888e;
     }
     .icon-message {
       color: #36a3f7;
