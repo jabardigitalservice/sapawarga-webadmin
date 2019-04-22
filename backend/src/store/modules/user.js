ce70f9a445c95f4fcdef91cfc0e39585a658de50
Yoga Hanggara <yohang88@gmail.com>
2019-04-22 08:43:23 +0700

    [Admin] Fix with Eslinter
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- backend/src/store/modules/user.js
+++ backend/src/store/modules/user.js
@@ -1,6 +1,6 @@
-import { login, logout, getInfo } from '@/api/user';
-import { getToken, setToken, removeToken } from '@/utils/auth';
-import router, { resetRouter } from '@/router';
+import { login, getInfo } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import router, { resetRouter } from '@/router'
 
 const state = {
   token: getToken(),
@@ -9,44 +9,44 @@ const state = {
   introduction: '',
   roles: [],
   dummy_image: require('@/assets/user.png')
-};
+}
 
 const mutations = {
   SET_TOKEN: (state, token) => {
-    state.token = token;
+    state.token = token
   },
   SET_INTRODUCTION: (state, introduction) => {
-    state.introduction = introduction;
+    state.introduction = introduction
   },
   SET_NAME: (state, name) => {
-    state.name = name;
+    state.name = name
   },
   SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar;
+    state.avatar = avatar
   },
   SET_ROLES: (state, roles) => {
-    state.roles = roles;
+    state.roles = roles
   }
-};
+}
 
 const actions = {
   // user login
   login({ commit }, userInfo) {
-    const { username, password } = userInfo;
+    const { username, password } = userInfo
 
     return new Promise((resolve, reject) => {
-      login({ LoginForm: { username: username.trim(), password: password } })
+      login({ LoginForm: { username: username.trim(), password: password }})
         .then(response => {
-          const { data } = response;
-          commit('SET_TOKEN', data.access_token);
-          setToken(data.access_token);
-          resolve();
+          const { data } = response
+          commit('SET_TOKEN', data.access_token)
+          setToken(data.access_token)
+          resolve()
         })
         .catch(error => {
-          error;
-          reject();
-        });
-    });
+          error
+          reject()
+        })
+    })
   },
 
   // get user info
@@ -54,87 +54,87 @@ const actions = {
     return new Promise((resolve, reject) => {
       getInfo(state.token)
         .then(response => {
-          let { data } = response;
-          let dummy_image = state.dummy_image;
+          let { data } = response
+          const dummy_image = state.dummy_image
 
           if (!data) {
-            reject('Verification failed, please Login again.');
+            reject('Verification failed, please Login again.')
           }
 
-          const { name, photo_url } = data;
+          const { name, photo_url } = data
 
-          const roles = ['admin'];
+          const roles = ['admin']
 
           // roles must be a non-empty array
           if (!roles || roles.length <= 0) {
-            reject('getInfo: roles must be a non-null array!');
+            reject('getInfo: roles must be a non-null array!')
           }
 
-          commit('SET_ROLES', roles);
-          commit('SET_NAME', name);
+          commit('SET_ROLES', roles)
+          commit('SET_NAME', name)
           if (photo_url == null) {
-            commit('SET_AVATAR', dummy_image);
+            commit('SET_AVATAR', dummy_image)
           } else {
-            commit('SET_AVATAR', photo_url);
+            commit('SET_AVATAR', photo_url)
           }
-          data = roles;
-          resolve(data);
+          data = roles
+          resolve(data)
         })
         .catch(error => {
-          reject(error);
-        });
-    });
+          reject(error)
+        })
+    })
   },
 
   // user logout
   logout({ commit }) {
     return new Promise((resolve, reject) => {
-      commit('SET_TOKEN', '');
-      commit('SET_ROLES', []);
-      removeToken();
-      resetRouter();
-      resolve();
-    });
+      commit('SET_TOKEN', '')
+      commit('SET_ROLES', [])
+      removeToken()
+      resetRouter()
+      resolve()
+    })
   },
 
   // remove token
   resetToken({ commit }) {
     return new Promise(resolve => {
-      commit('SET_TOKEN', '');
-      commit('SET_ROLES', []);
-      removeToken();
-      resolve();
-    });
+      commit('SET_TOKEN', '')
+      commit('SET_ROLES', [])
+      removeToken()
+      resolve()
+    })
   },
 
   // Dynamically modify permissions
   changeRoles({ commit, dispatch }, role) {
     return new Promise(async resolve => {
-      const token = role + '-token';
+      const token = role + '-token'
 
-      commit('SET_TOKEN', token);
-      setToken(token);
+      commit('SET_TOKEN', token)
+      setToken(token)
 
-      const { roles } = await dispatch('getInfo');
+      const { roles } = await dispatch('getInfo')
 
-      resetRouter();
+      resetRouter()
 
       // generate accessible routes map based on roles
       const accessRoutes = await dispatch('permission/generateRoutes', roles, {
         root: true
-      });
+      })
 
       // dynamically add accessible routes
-      router.addRoutes(accessRoutes);
+      router.addRoutes(accessRoutes)
 
-      resolve();
-    });
+      resolve()
+    })
   }
-};
+}
 
 export default {
   namespaced: true,
   state,
   mutations,
   actions
-};
+}
