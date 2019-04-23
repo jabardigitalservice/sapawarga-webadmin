f79c42209e0aee5ce154de5d094ab0395b91ff5d
Ganjar Setia <ganjar_setia@ymail.com>
2019-04-23 20:54:52 +0700

    update fixture total user using API response format

--- backend/tests/unit/fixtures/totalUser.js
+++ backend/tests/unit/fixtures/totalUser.js
@@ -1,9 +1,44 @@
 export default {
-  totalUser: 99,
-  totalUserProvince: 2,
-  totalUserKoKab: 27,
-  totalUserKec: 60,
-  totalUserKel: 70,
-  totalUserRw: 80,
-  totalUserRt: 50
-}
+  success: true,
+  status: 200,
+  data: {
+    items: [
+      {
+        id: 1,
+        level: "all",
+        name: "Semua",
+        value: 32
+      },
+      {
+        id: 2,
+        level: "prov",
+        name: "Provinsi",
+        value: 31
+      },
+      {
+        id: 3,
+        level: "kokab",
+        name: "Kota/Kab",
+        value: 27
+      },
+      {
+        id: 4,
+        level: "kec",
+        name: "Kecamatan",
+        value: 30
+      },
+      {
+        id: 5,
+        level: "kel",
+        name: "Kelurahan",
+        value: 29
+      },
+      {
+        id: 6,
+        level: "rw",
+        name: "RW",
+        value: 16
+      }
+    ]
+  }
+};
