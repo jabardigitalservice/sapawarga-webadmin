7e7eafc80c6b26ad390721b8d805c8e11d9ea645
Yoga Hanggara <yohang88@gmail.com>
2019-04-24 11:08:40 +0700

    [Admin] Sidebar menu using language
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- backend/src/lang/id.js
+++ backend/src/lang/id.js
@@ -1,4 +1,14 @@
 export default {
+  route: {
+    dashboard: 'Dashboard',
+    'user-manage': 'Kelola Pengguna',
+    'user-create': 'Tambah Pengguna Baru',
+    'user-list-provinsi': 'Provinsi',
+    'user-list-kabkota': 'Kabupaten / Kota',
+    'user-list-kecamatan': 'Kecamatan',
+    'user-list-kelurahan': 'Kelurahan',
+    'user-list-rw': 'User Tingkat RW'
+  },
   crud: {
     'activate-confirm': 'Anda yakin ingin mengaktifkan pengguna ini?',
     'deactivate-confirm': 'Anda yakin ingin menonaktifkan pengguna ini?',
