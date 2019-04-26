d4a00fc2468708944e9ef751dc6eaddc880d3562
aldidoanta <aldidkurnia@gmail.com>
2019-04-26 18:03:23 +0700

    [ci skip] [API] [PhoneBook] SQL to get nearest phonebook by radius

---
+++ api/models/scripts/getNearestByRadius.sql
@@ -0,0 +1,28 @@
+SELECT id, name,
+       latitude, longitude, distance
+  FROM (
+ SELECT pb.id,
+        pb.name,
+        pb.latitude, pb.longitude,
+        c.radius,
+        c.distance_unit
+                 * DEGREES(ACOS(COS(RADIANS(c.latpoint))
+                 * COS(RADIANS(pb.latitude))
+                 * COS(RADIANS(c.longpoint - pb.longitude))
+                 + SIN(RADIANS(c.latpoint))
+                 * SIN(RADIANS(pb.latitude)))) AS distance
+  FROM phonebooks AS pb
+  JOIN (   /* these are the query parameters */
+        SELECT  -6.910711 AS latpoint, 107.609822 AS longpoint,
+                3.0 AS radius, 111.045 AS distance_unit
+    ) AS c ON 1=1
+  WHERE pb.latitude
+     BETWEEN c.latpoint  - (c.radius / c.distance_unit)
+         AND c.latpoint  + (c.radius / c.distance_unit)
+    AND pb.longitude
+     BETWEEN c.longpoint - (c.radius / (c.distance_unit * COS(RADIANS(c.latpoint))))
+         AND c.longpoint + (c.radius / (c.distance_unit * COS(RADIANS(c.latpoint))))
+ ) AS d
+ WHERE distance <= radius
+ ORDER BY distance
+LIMIT 15
