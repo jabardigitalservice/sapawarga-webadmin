68f744a9ed845160f9fa09b4abd386982c8f3c4e
aldidoanta <aldidkurnia@gmail.com>
2019-05-03 16:36:06 +0700

    [API] [Category] Add migration and model class for Category

---
+++ api/models/Category.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "categories".
+ *
+ * @property int $id
+ * @property string $type
+ * @property string $name
+ * @property array $meta
+ * @property int $status
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    const STATUS_DELETED = -1;
+    const STATUS_DISABLED = 0;
+    const STATUS_ACTIVE = 10;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'categories';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['type', 'name'], 'string', 'max' => 64],
+            [['type', 'name'], 'trim'],
+            [['name', 'meta'], 'safe'],
+            [['type', 'name', 'status'], 'required'],
+            ['status', 'integer'],
+        ];
+    }
+
+    public function fields()
+    {
+        $fields = [
+            'id',
+            'type',
+            'name',
+            'meta',
+            'status',
+            'status_label' => function () {
+                $statusLabel = '';
+                switch ($this->status) {
+                    case self::STATUS_ACTIVE:
+                        $statusLabel = Yii::t('app', 'status.active');
+                        break;
+                    case self::STATUS_DISABLED:
+                        $statusLabel = Yii::t('app', 'status.inactive');
+                        break;
+                    case self::STATUS_DELETED:
+                        $statusLabel = Yii::t('app', 'status.deleted');
+                        break;
+                }
+                return $statusLabel;
+            },
+            'created_at',
+            'updated_at',
+        ];
+
+        return $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'type' => 'Type',
+            'name' => 'Name',
+            'meta' => 'Meta',
+            'status' => 'Status',
+        ];
+    }
+
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return [
+            [
+                'class'              => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value'              => time(),
+            ],
+        ];
+    }
+}
