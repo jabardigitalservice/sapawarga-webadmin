b7a6490130b0379878fbbdbf00f0dbe0fcb154eb
Ganjar Setia <ganjar_setia@ymail.com>
2019-05-08 11:16:13 +0700

    test gitlab runner #2

--- backend/src/views/nomorPenting/show.vue
+++ backend/src/views/nomorPenting/show.vue
@@ -3,7 +3,7 @@
     <el-row :gutter="10">
       <el-col :sm="24" :lg="8" :xl="6">
         <PhotoBox :image="imageUrl" :height="height" />
-        <!-- ini sample asli iframe <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.280178877438!2d108.20895931416229!3d-7.322392994715478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwMTknMjAuNiJTIDEwOMKwMTInNDAuMSJF!5e0!3m2!1sen!2sid!4v1557246825382!5m2!1sen!2sid" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+        <!-- ini sample asli iframe, dimana gagal! <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.280178877438!2d108.20895931416229!3d-7.322392994715478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwMTknMjAuNiJTIDEwOMKwMTInNDAuMSJF!5e0!3m2!1sen!2sid!4v1557246825382!5m2!1sen!2sid" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
         <!-- <iframe :src="urlMap" width="400" height="400" frameborder="0" style="border:0" allowfullscreen></iframe> -->
         <vue-friendly-iframe :src="urlMap" @load="onLoad" @document-load="onDocumentLoad" :style="{'border':0}"></vue-friendly-iframe>
       </el-col>
