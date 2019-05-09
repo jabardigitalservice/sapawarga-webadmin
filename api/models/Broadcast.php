70f6c59a96ce4340cd77b2082a3a95f5f7361cfb
aldidoanta <aldidkurnia@gmail.com>
2019-05-09 14:23:28 +0700

    [API] Revert change

--- api/models/Broadcast.php
+++ api/models/Broadcast.php
@@ -140,7 +140,7 @@ class Broadcast extends \yii\db\ActiveRecord
     }
 
     /**
-     * Checks if category_id is current user id
+     * Checks if category_id is current user's id
      *
      * @param $attribute
      * @param $params
