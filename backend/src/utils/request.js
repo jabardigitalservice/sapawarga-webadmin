039727f530ed5ae157f8906f8e536b4d16f86c92
Ma'ruf <maruf.harsono@gmail.com>
2019-05-09 11:24:15 +0700

    admin-fixBugLogin change error message

--- backend/src/utils/request.js
+++ backend/src/utils/request.js
@@ -74,7 +74,7 @@ service.interceptors.response.use(
   },
   error => {
     Message({
-      message: 'Tidak ada koneksi internet',
+      message: 'Oops, telah terjadi kesalahan, silahkan muat ulang halaman ini',
       type: 'error',
       duration: 5 * 1000
     })
