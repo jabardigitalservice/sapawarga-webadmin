4fa071e44aeffdb083fb3f394bc519caa3e60b32
Ma'ruf <maruf.harsono@gmail.com>
2019-05-10 17:27:03 +0700

    admin-view detail responsive

--- backend/src/components/PhotoBox/index.vue
+++ backend/src/components/PhotoBox/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="image-preview">
+  <div class="image-preview image">
     <img class="preview" :src="imageData">
   </div>
 </template>
@@ -33,39 +33,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.box-center {
-
-  // display: table;
-}
-
 img.preview {
-    width: 400px;
-    height: 350px;
-    background-color: white;
-    border: 1px solid #DDD;
-    margin-left: 20px;
-    -webkit-box-shadow: 0px 0px 30px -10px rgba(0,0,0,0.75);
-    -moz-box-shadow: 0px 0px 30px -10px rgba(0,0,0,0.75);
-    box-shadow: 0px 0px 30px -10px rgba(0,0,0,0.75);
+  width: 400px;
+  height: 350px;
+  border-radius: 5px;
+  margin-left: 20px;
+  -webkit-box-shadow: 0px 0px 30px -10px rgba(0,0,0,0.75);
+  -moz-box-shadow: 0px 0px 30px -10px rgba(0,0,0,0.75);
+  box-shadow: 0px 0px 30px -10px rgba(0,0,0,0.75);
+}
+
+@media only screen and (min-width: 1200px)  and (max-width: 1400px) {
 
 }
-// .box-card-header {
-//   position: relative;
-//   width: 600px;
-//   height: 400px;
-//   /* height: 220px; */
-//   img {
-//     // width: 100%;
-//     // height: 100%;
-//     // transition: all 0.2s linear;
-//     // &:hover {
-//     //   transform: scale(1.1, 1.1);
-//     //   filter: contrast(130%);
-//     width: 100%;
-//     height: 100%;
-//     background-color: white;
-//     border: 1px solid #DDD;
-//     }
-//   }
-
+@media only screen and (max-width: 1200px) {
+  .image {
+    // padding-left: 30%;
+    background-color: black;
+  }
+}
 </style>
