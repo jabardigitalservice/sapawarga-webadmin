b313e6dfb6cd09196437d8c24241de1b1e10246a
maruf12 <maruf.harsono@gmail.com>
2019-05-14 17:43:53 +0700

    admin-broadcast-new-message form broadcast

---
+++ backend/src/views/broadcast/components/FormBroadcast.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="components-container">
+        <p class="warn-content">Broadcast</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+ 
+
+</style>
