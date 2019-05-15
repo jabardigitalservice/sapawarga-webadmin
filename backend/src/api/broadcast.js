d228d56961b4aec60f1eee8dbcfe971e41486572
Yoga Hanggara <yohang88@gmail.com>
2019-05-15 14:21:50 +0700

    [Admin] [Broadcast] List page
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- backend/src/api/broadcast.js
+++ backend/src/api/broadcast.js
@@ -1,5 +1,13 @@
 import request from '@/utils/request'
 
+export function fetchList(query) {
+  return request({
+    url: '/broadcasts',
+    method: 'get',
+    params: query
+  })
+}
+
 export function create(data) {
   return request({
     url: '/broadcasts',
--- backend/src/views/broadcast/list.vue
+++ backend/src/views/broadcast/list.vue
@@ -1,3 +1,169 @@
 <template>
-  <h1>Hello this is list broadcast</h1>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :lg="6">
+        Kiri
+      </el-col>
+      <el-col :lg="18">
+        <el-table v-loading="listLoading" :data="list" border stripe fit highlight-current-row style="width: 100%" @sort-change="changeSort">
+          <el-table-column type="index" width="50" align="center" :index="getTableRowNumbering" />
+
+          <el-table-column prop="title" sortable="custom" label="Judul" />
+
+          <el-table-column prop="status" sortable="custom" class-name="status-col" label="Status" width="150px">
+            <template slot-scope="{row}">
+              <el-tag :type="row.status | statusFilter">
+                {{ row.status_label }}
+              </el-tag>
+            </template>
+          </el-table-column>
+
+          <el-table-column prop="created_at" sortable="custom" label="Dibuat" />
+          <el-table-column prop="updated_at" sortable="custom" label="Dikirim">
+            <template slot-scope="{row}">
+              {{ getSentDateTime(row) }}
+            </template>
+          </el-table-column>
+
+          <el-table-column align="center" label="Actions" width="250px">
+            <template slot-scope="scope">
+              <router-link :to="'/nomor-penting/show/'+scope.row.id">
+                <el-button type="white" size="mini">
+                  View
+                </el-button>
+              </router-link>
+              <router-link :to="'/nomor-penting/edit/'+scope.row.id">
+                <el-button type="white" size="mini">
+                  Edit
+                </el-button>
+              </router-link>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+      </el-col>
+    </el-row>
+  </div>
 </template>
+
+<script>
+import { fetchList } from '@/api/broadcast'
+import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+
+export default {
+
+  components: { Pagination },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        '10': 'success',
+        '0': 'info',
+        '-1': 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  props: {
+    roleId: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      list: null,
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        title: null,
+        sort_by: 'updated_at',
+        sort_order: 'descending',
+        page: 1,
+        limit: 10
+      }
+    }
+  },
+
+  created() {
+    this.getList()
+  },
+
+  methods: {
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data._meta.totalCount
+        this.listLoading = false
+      })
+    },
+
+    resetFilter() {
+      Object.assign(this.$data.listQuery, this.$options.data().listQuery)
+
+      this.getList()
+    },
+
+    getTableRowNumbering(index) {
+      return ((this.listQuery.page - 1) * this.listQuery.limit) + (index + 1)
+    },
+
+    getSentDateTime(row) {
+      return row.status === 10 ? row.updated_at : ''
+    },
+
+    changeSort(e) {
+      this.listQuery.sort_by = e.prop
+      this.listQuery.sort_order = e.order
+      this.getList()
+    },
+
+    async activateRecord(id) {
+      try {
+        await this.$confirm(this.$t('crud.deactivate-confirm'), 'Warning', {
+          confirmButtonText: this.$t('common.confirm'),
+          cancelButtonText: this.$t('common.cancel'),
+          type: 'warning'
+        })
+
+        this.listLoading = true
+
+        // await activate(id)
+
+        this.$message({
+          type: 'success',
+          message: this.$t('crud.activate-success')
+        })
+
+        this.getList()
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    async deactivateRecord(id) {
+      try {
+        await this.$confirm(this.$t('crud.deactivate-confirm'), 'Warning', {
+          confirmButtonText: this.$t('common.confirm'),
+          cancelButtonText: this.$t('common.cancel'),
+          type: 'warning'
+        })
+
+        this.listLoading = true
+
+        // await deactivate(id)
+
+        this.$message({
+          type: 'success',
+          message: this.$t('crud.deactivate-success')
+        })
+
+        this.getList()
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+}
+</script>
