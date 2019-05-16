6776e60dd03e3714f2ff423d3dad317b9f74718b
maruf12 <maruf.harsono@gmail.com>
2019-05-16 13:26:28 +0700

    admin-broadcast-edit edit component

---
+++ backend/src/views/broadcast/edit.vue
@@ -0,0 +1,11 @@
+<template>
+  <FormBroadcast :is-edit="true" />
+</template>
+<script>
+import FormBroadcast from './components/FormBroadcast'
+
+export default {
+  name: 'EditForm',
+  components: { FormBroadcast }
+}
+</script>
