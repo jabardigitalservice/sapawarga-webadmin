8d56856e6c43624bddec683e1d22044b85f09c2f
Yoga Hanggara <yohang88@gmail.com>
2019-05-16 15:07:10 +0700

    [Admin] [Broadcast] Fix column width
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- backend/src/views/broadcast/list.vue
+++ backend/src/views/broadcast/list.vue
@@ -18,7 +18,7 @@
         <el-table v-loading="listLoading" :data="list" border stripe fit highlight-current-row style="width: 100%" @sort-change="changeSort">
           <el-table-column type="index" width="50" align="center" :index="getTableRowNumbering" />
 
-          <el-table-column prop="title" sortable="custom" label="Judul" />
+          <el-table-column prop="title" sortable="custom" label="Judul" min-width="350" />
 
           <el-table-column prop="status" sortable="custom" class-name="status-col" label="Status" width="150px">
             <template slot-scope="{row}">
