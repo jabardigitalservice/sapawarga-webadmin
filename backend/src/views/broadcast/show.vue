88067b5efd470c2a972511cbddccbf20ccecb178
maruf12 <maruf.harsono@gmail.com>
2019-05-17 09:26:40 +0700

    admin-broadcast-view delete media query

--- backend/src/views/broadcast/show.vue
+++ backend/src/views/broadcast/show.vue
@@ -119,16 +119,4 @@ export default {
     margin-right: 20px !important
   }
 }
-
-@media only screen and (min-width: 1200px) and (max-width: 1570px) {
-  #map iframe {
-    width: 250px;
-    height: 200px;
-    border-radius: 5px;
-    margin-left: 20px;
-    -webkit-box-shadow: 0px 0px 25px -10px rgba(0,0,0,0.75);
-    -moz-box-shadow: 0px 0px 25px -10px rgba(0,0,0,0.75);
-    box-shadow: 0px 0px 25px -10px rgba(0,0,0,0.75);
-  }
-}
 </style>
