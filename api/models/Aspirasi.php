c33c46e911a5b1d18a0eed26cb0f42bf862898b8
Yoga Hanggara <yohang88@gmail.com>
2019-05-17 11:22:59 +0700

    [API] [Aspirasi] setup model [ci skip]
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- api/models/Aspirasi.php
+++ api/models/Aspirasi.php
@@ -9,8 +9,180 @@ use yii\behaviors\TimestampBehavior;
  * This is the model class for table "aspirasi".
  *
  * @property int $id
+ * @property int $author_id
+ * @property int $category_id
+ * @property string $title
+ * @property string $description
+ * @property int $kabkota_id
+ * @property int $kec_id
+ * @property int $kel_id
+ * @property string $rw
+ * @property mixed $attachments
+ * @property mixed $meta
+ * @property int $status
  */
 class Aspirasi extends \yii\db\ActiveRecord
 {
-    //
+    const STATUS_DELETED = -1;
+    const STATUS_DRAFT = 0;
+    const STATUS_APPROVAL_PENDING = 5;
+    const STATUS_APPROVAL_REJECTED = 3;
+    const STATUS_PUBLISHED = 10;
+
+    const CATEGORY_TYPE = 'aspirasi';
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'aspirasi';
+    }
+
+    public function getAuthor()
+    {
+        return $this->hasOne(User::class, ['id' => 'author_id']);
+    }
+
+    public function getCategory()
+    {
+        return $this->hasOne(Category::class, ['id' => 'category_id']);
+    }
+
+    public function getKelurahan()
+    {
+        return $this->hasOne(Area::className(), ['id' => 'kel_id']);
+    }
+
+    public function getKecamatan()
+    {
+        return $this->hasOne(Area::className(), ['id' => 'kec_id']);
+    }
+
+    public function getKabkota()
+    {
+        return $this->hasOne(Area::className(), ['id' => 'kabkota_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'status'], 'required'],
+            [['title', 'description', 'rw', 'meta'], 'trim'],
+            ['title', 'string', 'max' => 255],
+            ['rw', 'default'],
+            ['attachments', 'default'],
+            [['author_id', 'category_id', 'kabkota_id', 'kec_id', 'kel_id', 'status'], 'integer'],
+            ['meta', 'default'],
+        ];
+    }
+
+    public function fields()
+    {
+        $fields = [
+            'id',
+            'author_id',
+            'author' => function () {
+                return [
+                    'id'            => $this->author->id,
+                    'name'          => $this->author->name,
+                    'role_label'    => $this->author->getRoleLabel(),
+                ];
+            },
+            'category_id',
+            'category' => function () {
+                return [
+                    'id'   => $this->category->id,
+                    'name' => $this->category->name,
+                ];
+            },
+            'title',
+            'description',
+            'kabkota_id',
+            'kabkota' => function () {
+                if ($this->kabkota) {
+                    return [
+                        'id'   => $this->kabkota->id,
+                        'name' => $this->kabkota->name,
+                    ];
+                } else {
+                    return null;
+                }
+            },
+            'kec_id',
+            'kecamatan' => function () {
+                if ($this->kecamatan) {
+                    return [
+                        'id'   => $this->kecamatan->id,
+                        'name' => $this->kecamatan->name,
+                    ];
+                } else {
+                    return null;
+                }
+            },
+            'kel_id',
+            'kelurahan' => function () {
+                if ($this->kelurahan) {
+                    return [
+                        'id'   => $this->kelurahan->id,
+                        'name' => $this->kelurahan->name,
+                    ];
+                } else {
+                    return null;
+                }
+            },
+            'rw',
+            'meta',
+            'status',
+            'status_label' => function () {
+                $statusLabel = '';
+                switch ($this->status) {
+                    case self::STATUS_PUBLISHED:
+                        $statusLabel = Yii::t('app', 'status.published');
+                        break;
+                    case self::STATUS_DRAFT:
+                        $statusLabel = Yii::t('app', 'status.draft');
+                        break;
+                    case self::STATUS_DELETED:
+                        $statusLabel = Yii::t('app', 'status.deleted');
+                        break;
+                    case self::STATUS_APPROVAL_PENDING:
+                        $statusLabel = Yii::t('app', 'status.approval-pending');
+                        break;
+                    case self::STATUS_APPROVAL_REJECTED:
+                        $statusLabel = Yii::t('app', 'status.approval-rejected');
+                        break;
+                }
+                return $statusLabel;
+            },
+            'created_at',
+            'updated_at',
+        ];
+
+        return $fields;
+    }
+
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return [
+            [
+                'class'              => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value'              => time(),
+            ],
+        ];
+    }
+
+    /** @inheritdoc */
+    public function beforeSave($insert)
+    {
+        $this->author_id = Yii::$app->user->getId();
+
+        return parent::beforeSave($insert);
+    }
 }
