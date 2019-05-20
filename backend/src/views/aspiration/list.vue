67abe08183776def134eefa2891ab44995ee3d0b
maruf12 <maruf.harsono@gmail.com>
2019-05-20 15:02:18 +0700

    admin-aspiration-list

---
+++ backend/src/views/aspiration/list.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :lg="24">
+        <el-row style="margin: 10px 0px">
+          <el-col :span="24">
+            <router-link :to="{ path: '/aspirasi/create'}">
+              <el-button type="primary" size="small" icon="el-icon-plus">
+                Tambah Pesan Baru
+              </el-button>
+            </router-link>
+          </el-col>
+        </el-row>
+
+        <el-table v-loading="listLoading" :data="list" border stripe fit highlight-current-row style="width; 100%" @sort-change="changeSort">
+          <el-table-column type="index" width="50" align="center" :index="getTableRowNumbering" />
+
+          <el-table-column prop="title" sortable="custom" label="Judul Aspirasi" min-width="200" />
+
+          <el-table-column prop="category.name" sortable="custom" label="Kategori" min-width="100" />
+
+          <el-table-column prop="status" sortable="custom" class-name="status-col" label="Status" width="150px">
+            <template slot-scope="{row}">
+              <el-tag :type="row.status | statusFilter">
+                {{ row.status_label }}
+              </el-tag>
+            </template>
+          </el-table-column>
+
+          <el-table-column prop="created_at" sortable="custom" label="Dibuat" width="150">
+            <template slot-scope="{row}">
+              {{ row.created_at | moment('D MMMM YYYY HH:mm') }}
+            </template>
+          </el-table-column>
+
+          <el-table-column align="center" label="Actions" width="150px">
+            <template slot-scope="scope">
+              <router-link :to="'/broadcast/show/'+scope.row.id">
+                <el-button type="white" size="medium">
+                  Lihat
+                </el-button>
+              </router-link>
+            </template>
+          </el-table-column>
+        </el-table>
+        <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { fetchList } from '@/api/aspiration'
+import Pagination from '@/components/Pagination'
+
+export default {
+  components: { Pagination },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        '10': 'success',
+        '0': 'info',
+        '-1': 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  props: {
+    roleId: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      list: null,
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        title: null,
+        page: 1,
+        limit: 10
+      }
+    }
+  },
+  created() {
+    this.getList()
+  },
+
+  methods: {
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data._meta.totalCount
+        this.listLoading = false
+      })
+    },
+
+    resetFilter() {
+      Object.assign(this.$data.listQuery, this.$options.data().listQuery)
+
+      this.getList()
+    },
+
+    getTableRowNumbering(index) {
+      return ((this.listQuery.page - 1) * this.listQuery.limit) + (index + 1)
+    },
+
+    changeSort(e) {
+      this.listQuery.sort_by = e.prop
+      this.listQuery.sort_order = e.order
+      this.getList()
+    }
+  }
+
+}
+</script>
