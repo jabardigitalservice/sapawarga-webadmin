6ca37024941d089abf6fe5436cbc2f0f2aa2fa16
maruf12 <maruf.harsono@gmail.com>
2019-05-22 16:42:49 +0700

    admin-profile map

--- backend/src/views/profile/index.vue
+++ backend/src/views/profile/index.vue
@@ -10,6 +10,11 @@
             <iframe
               id="gmap_canvas"
               :src="`https://maps.google.com/maps?q=${latitude},${longitude}&t=&z=16&ie=UTF8&iwloc=&output=embed`"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+            ></iframe>
           </div>
         </div>
       </el-col>
