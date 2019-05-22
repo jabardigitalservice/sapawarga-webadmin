29d1d88789ae31d32784389997cd9cc8eb587e61
aldidoanta <aldidkurnia@gmail.com>
2019-05-22 10:31:50 +0700

    [API] [Notification] Add test case for notification endpoint [ci skip]

---
+++ api/tests/api/NotificationCest.php
@@ -0,0 +1,43 @@
+<?php
+
+class NotificationCest
+{
+    private $endpointNotification = '/v1/notification';
+
+    // Test cases for users
+    public function userCannotCreateNotification(ApiTester $I)
+    {
+        $I->amUser();
+
+        $I->sendPOST($this->endpointNotification);
+        $I->canSeeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 403,
+        ]);
+    }
+
+    // Test cases for admins
+    public function createNewNotification(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendPOST($this->endpointNotification, [
+            'title'      => 'Title',
+            'description'      => 'Description',
+            'data'    => [
+                'push_notification' => true,
+            ],
+        ]);
+
+        $I->canSeeResponseCodeIs(201);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 201,
+        ]);
+    }
+}
