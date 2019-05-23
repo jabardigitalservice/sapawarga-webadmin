dd873a532b16dd004c844149d7b94257b43e1ef8
maruf12 <maruf.harsono@gmail.com>
2019-05-23 09:55:33 +0700

    admin-profile fix linter

--- backend/src/views/profile/index.vue
+++ backend/src/views/profile/index.vue
@@ -3,7 +3,7 @@
     <p class="warn-content">Profile Admin</p>
     <el-row :gutter="10">
       <el-col class="col-left" :xs="24" :sm="24" :md="24" :lg="7" :xl="7">
-        <PhotoBox class="image" :image="imageUrl" :height="height"/>
+        <PhotoBox class="image" :image="imageUrl" :height="height" />
         <p class="warn-content map-title">Lokasi Anda</p>
         <div class="mapouter">
           <div class="gmap_canvas">
@@ -14,50 +14,50 @@
               scrolling="no"
               marginheight="0"
               marginwidth="0"
-            ></iframe>
+            />
           </div>
         </div>
       </el-col>
       <el-col class="col-right" :xs="24" :sm="24" :md="24" :lg="17" :xl="17">
-          <el-table stripe :data="tableData" :show-header="false" border style="width: 100%">
-            <el-table-column prop="title" width="180"/>
-            <el-table-column prop="content"/>
+        <el-table stripe :data="tableData" :show-header="false" border style="width: 100%">
+          <el-table-column prop="title" width="180" />
+          <el-table-column prop="content" />
+        </el-table>
+        <div class="social-media">
+          <p class="warn-content">Media Sosial</p>
+          <el-table stripe :data="twitterIcon" :show-header="false" border style="width: 100%">
+            <el-table-column width="180">
+              <a :href="`https://twitter.com/${twitterAccount}`" target="_blank">
+                <img class="social-media-admin twitter" :src="twitter" align="middle">
+              </a>
+            </el-table-column>
+            <el-table-column prop="content" />
           </el-table>
-          <div class="social-media">
-            <p class="warn-content">Media Sosial</p>
-            <el-table stripe :data="twitterIcon" :show-header="false" border style="width: 100%">
-              <el-table-column width="180">
-                <a :href="`https://twitter.com/${twitterAccount}`" target="_blank">
-                  <img class="social-media-admin twitter" :src="twitter" align="middle">
-                </a>
-              </el-table-column>
-              <el-table-column prop="content"/>
-            </el-table>
-            <el-table stripe :data="instagramIcon" :show-header="false" border style="width: 100%">
-              <el-table-column width="180">
-                <a :href="`https://www.instagram.com/${instagramAccount}`" target="_blank">
-                  <img class="social-media-admin twitter" :src="instagram">
-                </a>
-              </el-table-column>
-              <el-table-column prop="content"/>
-            </el-table>
-            <el-table stripe :data="facebookIcon" :show-header="false" border style="width: 100%">
-              <el-table-column width="180">
-                <a :href="`${facebookAccount}`" target="_blank">
-                  <img class="social-media-admin twitter" :src="facebook">
-                </a>
-              </el-table-column>
-              <el-table-column prop="content"/>
-            </el-table>
-          </div>
+          <el-table stripe :data="instagramIcon" :show-header="false" border style="width: 100%">
+            <el-table-column width="180">
+              <a :href="`https://www.instagram.com/${instagramAccount}`" target="_blank">
+                <img class="social-media-admin twitter" :src="instagram">
+              </a>
+            </el-table-column>
+            <el-table-column prop="content" />
+          </el-table>
+          <el-table stripe :data="facebookIcon" :show-header="false" border style="width: 100%">
+            <el-table-column width="180">
+              <a :href="`${facebookAccount}`" target="_blank">
+                <img class="social-media-admin twitter" :src="facebook">
+              </a>
+            </el-table-column>
+            <el-table-column prop="content" />
+          </el-table>
+        </div>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import PhotoBox from "@/components/PhotoBox";
-import { getInfo } from "@/api/user";
+import PhotoBox from '@/components/PhotoBox'
+import { getInfo } from '@/api/user'
 
 export default {
   components: { PhotoBox },
@@ -65,7 +65,7 @@ export default {
     return {
       id: 0,
       imageUrl: null,
-      height: "220px",
+      height: '220px',
       tableData: [],
       instagramIcon: [],
       twitterIcon: [],
@@ -73,17 +73,17 @@ export default {
       urlMap: null,
       latitude: null,
       longitude: null,
-      twitter: require("@/assets/twitter.svg"),
-      facebook: require("@/assets/facebook.svg"),
-      instagram: require("@/assets/instagram.svg"),
+      twitter: require('@/assets/twitter.svg'),
+      facebook: require('@/assets/facebook.svg'),
+      instagram: require('@/assets/instagram.svg'),
       twitterAccount: null,
       facebookAccount: null,
       instagramAccount: null
-    };
+    }
   },
 
   created() {
-    this.getDetail();
+    this.getDetail()
   },
 
   methods: {
@@ -107,78 +107,78 @@ export default {
           twitter,
           username,
           role_label
-        } = response.data;
-        this.twitterAccount = twitter || "-";
-        this.facebookAccount = facebook || "-";
-        this.instagramAccount = instagram || "-";
-        this.imageUrl = photo_url !== null ? photo_url : null;
-        this.latitude = lat;
-        this.longitude = lon;
+        } = response.data
+        this.twitterAccount = twitter || '-'
+        this.facebookAccount = facebook || '-'
+        this.instagramAccount = instagram || '-'
+        this.imageUrl = photo_url !== null ? photo_url : null
+        this.latitude = lat
+        this.longitude = lon
         this.instagramIcon = [
           {
-            content: ": " + (instagram !== null ? instagram : '-')
+            content: ': ' + (instagram !== null ? instagram : '-')
           }
         ]
         this.twitterIcon = [
           {
-            content: ": " + (twitter !== null ? twitter : '-')
+            content: ': ' + (twitter !== null ? twitter : '-')
           }
         ]
         this.facebookIcon = [
           {
-            content: ": " + (facebook !== null ? facebook : '-')
+            content: ': ' + (facebook !== null ? facebook : '-')
           }
         ]
         this.tableData = [
           {
-            title: "Nama",
-            content: ": " + (name !== null ? name : "-")
+            title: 'Nama',
+            content: ': ' + (name !== null ? name : '-')
           },
           {
-            title: "Username",
-            content: ": " + (username !== null ? username : "-")
+            title: 'Username',
+            content: ': ' + (username !== null ? username : '-')
           },
           {
-            title: "Email",
-            content: ": " + (email !== null ? email : "-")
+            title: 'Email',
+            content: ': ' + (email !== null ? email : '-')
           },
           {
-            title: "Telepon",
-            content: ": " + (phone !== null ? phone : "-")
+            title: 'Telepon',
+            content: ': ' + (phone !== null ? phone : '-')
           },
           {
-            title: "Alamat Instansi",
-            content: ": " + (address !== null ? address : "-")
+            title: 'Alamat Instansi',
+            content: ': ' + (address !== null ? address : '-')
           },
           {
-            title: "RT",
-            content: ": " + (rt !== null ? rw : "-")
+            title: 'RT',
+            content: ': ' + (rt !== null ? rw : '-')
           },
           {
-            title: "RW",
-            content: ": " + (rw !== null ? rw : "-")
+            title: 'RW',
+            content: ': ' + (rw !== null ? rw : '-')
           },
           {
-            title: "Kelurahan",
-            content: ": " + (kelurahan !== null ? kelurahan.name : "-")
+            title: 'Kelurahan',
+            content: ': ' + (kelurahan !== null ? kelurahan.name : '-')
           },
           {
-            title: "Kecamatan",
-            content: ": " + (kecamatan !== null ? kecamatan.name : "-")
+            title: 'Kecamatan',
+            content: ': ' + (kecamatan !== null ? kecamatan.name : '-')
           },
           {
-            title: "Kab/Kota",
-            content: ": " + (kabkota !== null ? kabkota.name : "-")
+            title: 'Kab/Kota',
+            content: ': ' + (kabkota !== null ? kabkota.name : '-')
           },
           {
-            title: "Peran",
-            content: ": " + (role_label !== null ? role_label : "-")
-          },
-        ];
-      });
+            title: 'Peran',
+            content: ': ' + (role_label !== null ? role_label : '-')
+          }
+        ]
+      })
     }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
@@ -210,11 +210,6 @@ export default {
       height: 350px;
     }
   }
-
-.warn-content {
-  // color: #909399;
-  // background-color: #dcdfe6;
-}
 
 .map-title {
   width: 400px;
