e655ae76ee6d6a55077b74f9e589a7ee6e053a65
maruf12 <maruf.harsono@gmail.com>
2019-06-01 09:44:20 +0700

    admin-userManagement delete role latitude and longitude

--- backend/src/views/user/components/FormUser.vue
+++ backend/src/views/user/components/FormUser.vue
@@ -480,38 +480,6 @@ export default {
           { validator: validatorCoordinateRequired, trigger: 'change' },
           { validator: validatorCoordinateInputNumber, trigger: 'change' }
         ],
-        latitude: [
-          // {
-          //   required: true,
-          //   message: 'Latitude harus diisi, jika tidak ada latitude isi dengan -',
-          //   trigger: 'blur'
-          // },
-          {
-            pattern: /^[0-9.+-]+$/,
-            message: 'Latitude hanya boleh menggunakan angka, titik, - atau +',
-            trigger: 'blur'
-          },
-          {
-            validator: validatorCoordinateRequired,
-            trigger: 'change'
-          }
-        ],
-        longitude: [
-          // {
-          //   required: true,
-          //   message: 'Longitude harus diisi, jika tidak ada longitude isi dengan -',
-          //   trigger: 'blur'
-          // },
-          {
-            pattern: /^[0-9.+-]+$/,
-            message: 'Longitude hanya boleh menggunakan angka, titik, - atau +',
-            trigger: 'blur'
-          },
-          {
-            validator: validatorCoordinateRequired,
-            trigger: 'change'
-          }
-        ],
         kabkota: [
           {
             required: true,
