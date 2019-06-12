ff6f6a1cb91949ee16749d23e7aed7d5304965a3
aldidoanta <aldidkurnia@gmail.com>
2019-06-12 16:27:10 +0700

    [API][Notification] Add unit and API test, copied from Broadcast [ci skip]

--- api/tests/api/NotificationCest.php
+++ api/tests/api/NotificationCest.php
@@ -5,7 +5,35 @@ class NotificationCest
     private $endpointNotification = '/v1/notifications';
 
     // Test cases for users
-    public function userCannotCreateNotification(ApiTester $I)
+    public function getNotificationListBandung(ApiTester $I)
+    {
+        $I->amUser('user.bandung');
+
+        $I->sendGET($this->endpointNotification);
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+    }
+
+    public function getNotificationListBekasi(ApiTester $I)
+    {
+        $I->amUser('user.bekasi');
+
+        $I->sendGET($this->endpointNotification);
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+    }
+
+    public function userCannotCreateNewTest(ApiTester $I)
     {
         $I->amUser();
 
@@ -17,5 +45,215 @@ class NotificationCest
             'success' => false,
             'status'  => 403,
         ]);
+    }
+
+    public function userCannotUpdateTest(ApiTester $I)
+    {
+        $I->amUser();
+
+        $I->sendPUT("{$this->endpointNotification}/0");
+        $I->canSeeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 403,
+        ]);
+    }
+
+    public function userCannotDeleteTest(ApiTester $I)
+    {
+        $I->amUser();
+
+        $I->sendDELETE("{$this->endpointNotification}/0");
+        $I->canSeeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 403,
+        ]);
+    }
+
+
+    // Test cases for admins
+
+    public function createNewNotificationCategoryInvalid(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendPOST($this->endpointNotification, [
+            'author_id' => 1,
+            'category_id' => 0,
+            'title' => 'Notification Title',
+            'description' => 'Notification Description',
+            'kabkota_id' => null,
+            'kec_id' => null,
+            'kel_id' => null,
+            'rw' => null,
+            'status' => 10,
+        ]);
+
+        $I->canSeeResponseCodeIs(422);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 422,
+        ]);
+    }
+
+    public function getNotificationListAll(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendGET($this->endpointNotification);
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $I->seeResponseContainsJson([
+            'kabkota_id' => 22,
+        ]);
+
+        $I->seeResponseContainsJson([
+            'kabkota_id' => 23,
+        ]);
+    }
+
+    public function getNotificationListFilterCategory(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendGET("{$this->endpointNotification}?category_id=5");
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $I->seeResponseContainsJson([
+            'category_id' => 5,
+        ]);
+
+        $I->cantSeeResponseContainsJson([
+            'category_id' => 6,
+        ]);
+
+        $I->cantSeeResponseContainsJson([
+            'category_id' => 7,
+        ]);
+    }
+
+    public function getNotificationListFilterStatus(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendGET("{$this->endpointNotification}?status=10");
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $accessToken = $I->grabDataFromResponseByJsonPath('$.data.items');
+
+        $I->seeResponseContainsJson([
+            'data' => [
+                'items' => [
+                    [
+                        'status' => 10,
+                    ],
+                ],
+            ],
+        ]);
+
+        $I->cantSeeResponseContainsJson([
+            'data' => [
+                'items' => [
+                    [
+                        'status' => 0,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function getNotificationListSearchTitle(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendGET("{$this->endpointNotification}?search=Kegiatan");
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+    }
+
+    public function getNotificationItemNotFound(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendGET("{$this->endpointNotification}/0");
+        $I->canSeeResponseCodeIs(404);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 404,
+        ]);
+    }
+
+    public function getNotificationItem(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendGET("{$this->endpointNotification}/1");
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success'   => true,
+            'status'    => 200,
+            'data'      => [
+                'id' => 1,
+            ]
+        ]);
+    }
+
+    public function updateNotification(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendPUT("{$this->endpointNotification}/1", [
+            'title' => 'Edited',
+        ]);
+
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+    }
+
+    public function deleteNotification(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendDELETE("{$this->endpointNotification}/1");
+        $I->canSeeResponseCodeIs(204);
     }
 }
---
+++ api/tests/unit/models/NotificationTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace tests\unit\models;
+
+use app\models\Notification;
+
+class NotificationTest extends \Codeception\Test\Unit
+{
+    public function testValidateFillRequired()
+    {
+        $model = new Notification();
+
+        $this->assertFalse($model->validate());
+
+        $model->title       = 'test';
+        $model->description = 'test';
+
+        $model->kabkota_id  = 1;
+        $model->kec_id      = 1;
+        $model->kel_id      = 1;
+        $model->rw          = '001';
+        $model->status      = 10;
+
+        $this->assertTrue($model->validate());
+    }
+
+    public function testValidateRequired()
+    {
+        $model = new Notification();
+
+        $model->title       = null;
+        $model->description = null;
+
+        $model->kabkota_id  = null;
+        $model->kec_id      = null;
+        $model->kel_id      = null;
+        $model->rw          = null;
+        $model->status      = null;
+
+        $this->assertFalse($model->validate());
+
+        $this->assertTrue($model->hasErrors('title'));
+        $this->assertTrue($model->hasErrors('status'));
+
+        $this->assertFalse($model->hasErrors('description'));
+        $this->assertFalse($model->hasErrors('kabkota_id'));
+        $this->assertFalse($model->hasErrors('kec_id'));
+        $this->assertFalse($model->hasErrors('kel_id'));
+        $this->assertFalse($model->hasErrors('rw'));
+    }
+
+    public function testTitleTooLong()
+    {
+        $model        = new Notification();
+        $model->title = 'l5WdQPtZ8J6pEf1vZl2eynOuFP4g8qHXXeyYvE4h0y72QCesFoSyQUGqmVKpbv3tqeqITczwX1DLIFKdX6CjrCoNbJiOGQXzaDhRB5Q45wtPl9OaZDa6dz4jzk58GsXQfAiihEGd9nKCudaoXSBqxf2AITfSlAJ0kH2KaikUBOLrMHniFBPbIs2byQTPVqQ8FbwfJoxjJyga4oSHxPQlDVdgb9DQ1LI3fIMklRHc4itLdlsmOWBxcEVw47nPoIQ6';
+
+        $model->validate();
+
+        $this->assertTrue($model->hasErrors('title'));
+    }
+
+    public function testTitleMaxCharactersValid()
+    {
+        $model        = new Notification();
+        $model->title = 'Notification Title';
+        $model->validate();
+
+        $this->assertFalse($model->hasErrors('title'));
+
+        $model->title = 'l5WdQPtZ8J6pEf1vZl2eynOuFP4g8qHXXeyYvE4h0y72QCesFoSyQUGqmVKpbv3tqeqITczwX1DLIFKdX6CjrCoNbJiOGQXzaDhRB5Q45wtPl9OaZDa6dz4jzk58GsXQfAiihEGd9nKCudaoXSBqxf2AITfSlAJ0kH2KaikUBOLrMHniFBPbIs2byQTPVqQ8FbwfJoxjJyga4oSHxPQlDVdgb9DQ1LI3fIMklRHc4itLdlsmOWBxcEVw47nPoIQ';
+        $model->validate();
+
+        $this->assertFalse($model->hasErrors('title'));
+    }
+
+    public function testRWValidation()
+    {
+        $model       = new Notification();
+        $model->rw   = '1';
+        $model->validate();
+        $this->assertTrue($model->hasErrors('rw'));
+
+        $model->rw   = '0001';
+        $model->validate();
+        $this->assertTrue($model->hasErrors('rw'));
+
+        $model->rw   = '_a1A.';
+        $model->validate();
+        $this->assertTrue($model->hasErrors('rw'));
+
+        $model->rw   = '001';
+        $model->validate();
+        $this->assertFalse($model->hasErrors('rw'));
+    }
+}
