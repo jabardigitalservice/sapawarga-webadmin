f9645ef0219faf51771ad4f788f8b8ff36e2ef23
maruf12 <maruf.harsono@gmail.com>
2019-06-12 16:57:06 +0700

    admin-polling-list add api polling

---
+++ backend/src/api/polling.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+
+export function fetchList(query) {
+  return request({
+    url: 'http://35.247.135.93:3000/api/v1/polling',
+    method: 'get',
+    params: query
+  })
+}
+
+export function fetchRecord(id) {
+  return request({
+    url: `/polling/${id}`,
+    method: 'get'
+  })
+}
+
+export function create(data) {
+  return request({
+    url: '/polling',
+    method: 'post',
+    data
+  })
+}
+
+export function update(id, data) {
+  return request({
+    url: `/polling/${id}`,
+    method: 'put',
+    data
+  })
+}
