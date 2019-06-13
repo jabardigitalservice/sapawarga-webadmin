fcb81e61a4280da2edc3d006669bc694f661a8fa
aldidoanta <aldidkurnia@gmail.com>
2019-06-13 16:49:09 +0700

    [API][Notification] Minor fix for Notification seed data

--- api/migrations/seeder/notification.sql
+++ api/migrations/seeder/notification.sql
@@ -6,4 +6,4 @@ VALUES
     (1,16,'Draft Title','Draft description.',22,NULL,NULL,NULL,NULL,0,1557738743,1557738743),
     (1,16,'Deleted Title','Deleted description.',22,NULL,NULL,NULL,NULL,-1,1557738743,1557738743),
     (1,14,'Survey Taman di Kota Bandung','Untuk warga Kota Bandung, mari bantu kami untuk mewujudkan taman Kota Bandung yang lebih bersih dan nyaman melalui survey ini.',22,NULL,NULL,NULL,NULL,10,1557738743,1557738743),
-    (1,14,'Polling Kebijakan Kepemilikan Garasi di Kota Bekasi','Sebagai warga Kota Bekasi, apakah Anda setuju jika pemilik kendaraan bermotor diwajibkan untuk mempunyai garasi?',23,NULL,NULL,NULL,NULL,10,1557738743,1557738743);
+    (1,15,'Polling Kebijakan Kepemilikan Garasi di Kota Bekasi','Sebagai warga Kota Bekasi, apakah Anda setuju jika pemilik kendaraan bermotor diwajibkan untuk mempunyai garasi?',23,NULL,NULL,NULL,NULL,10,1557738743,1557738743);
