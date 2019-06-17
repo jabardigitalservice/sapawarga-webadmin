9f5aec2cf0645982dc79900e3dfffc55a9fad9f6
Ma'ruf <maruf.harsono@gmail.com>
2019-06-17 14:01:20 +0700

    admin-polling-detail change url api from mockup to dev server

--- backend/src/api/polling.js
+++ backend/src/api/polling.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function fetchList(query) {
   return request({
-    url: 'http://35.247.135.93:3000/api/v1/polling',
+    url: '/polling',
     method: 'get',
     params: query
   })
@@ -10,7 +10,7 @@ export function fetchList(query) {
 
 export function fetchRecord(id) {
   return request({
-    url: `http://35.247.135.93:3000/api/v1/polling/${id}`,
+    url: `/polling/${id}`,
     method: 'get'
   })
 }
