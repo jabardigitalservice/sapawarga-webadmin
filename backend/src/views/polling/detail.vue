b107b596d8969b65232cec48fc287bb5984620f3
Ma'ruf <maruf.harsono@gmail.com>
2019-06-17 14:00:05 +0700

    admin-polling-detail call logArrayAnswers function

--- backend/src/views/polling/detail.vue
+++ backend/src/views/polling/detail.vue
@@ -129,7 +129,7 @@ export default {
             content: question || '-'
           },
           {
-            content: answers.map(logArrayElements)
+            content: answers.map(logArrayAnswers)
           }
         ]
       })
