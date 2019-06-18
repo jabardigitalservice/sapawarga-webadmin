7007edb99f62c2c5760683fa17bdb7ae1ef847e1
Ganjar Setia <ganjar_setia@ymail.com>
2019-06-18 11:52:22 +0700

    edit notification

---
+++ backend/src/views/notification/edit.vue
@@ -0,0 +1,11 @@
+<template>
+  <FormNotification :is-edit="true" />
+</template>
+<script>
+import FormNotification from './components/FormNotification'
+
+export default {
+  name: 'EditForm',
+  components: { FormNotification }
+}
+</script>
