087babcbb32b041579939d8c4df24f247d02f642
Yoga Hanggara <yohang88@gmail.com>
2019-06-18 11:48:17 +0700

    [Survey] [API] API test [ci skip]
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

---
+++ api/tests/api/SurveyCest.php
@@ -0,0 +1,141 @@
+<?php
+
+class SurveyCest
+{
+    public function _before(ApiTester $I)
+    {
+        //
+    }
+
+    public function getListTest(ApiTester $I)
+    {
+        $I->amUser('survey');
+
+        $I->sendGET('/v1/survey');
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+    }
+
+    public function getShowTest(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendGET('/v1/survey/1');
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+    }
+
+    public function postCreateUnauthorizedTest(ApiTester $I)
+    {
+        $I->amUser('user');
+
+        $data = [];
+
+        $I->sendPOST('/v1/survey', $data);
+        $I->canSeeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 403,
+        ]);
+    }
+
+    public function postCreateTest(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $data = [
+            'title'        => 'Lorem ipsum',
+            'external_url' => 'http://google.com',
+            'category_id'  => 20,
+        ];
+
+        $I->sendPOST('/v1/survey', $data);
+        $I->canSeeResponseCodeIs(201);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 201,
+        ]);
+
+        $I->seeInDatabase('survey', [
+            'id'           => 1,
+            'title'        => 'Lorem ipsum',
+            'external_url' => 'http://google.com',
+            'category_id'  => 20,
+        ]);
+    }
+
+    public function postUpdateUnauthorizedTest(ApiTester $I)
+    {
+        $I->amUser('user');
+
+        $data = [];
+
+        $I->sendPUT('/v1/survey/1', $data);
+        $I->canSeeResponseCodeIs(403);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => false,
+            'status'  => 403,
+        ]);
+    }
+
+    public function postUpdateTest(ApiTester $I)
+    {
+        $I->amUser('user');
+
+        $data = [
+            'title'        => 'Lorem ipsum',
+            'external_url' => 'http://google.com',
+            'category_id'  => 20,
+        ];
+
+        $I->sendPUT('/v1/survey/1', $data);
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'success' => true,
+            'status'  => 200,
+        ]);
+
+        $I->seeInDatabase('survey', [
+            'id'           => 1,
+            'title'        => 'Lorem ipsum updated',
+            'external_url' => 'http://google-updated.com',
+            'category_id'  => 20,
+        ]);
+    }
+
+    public function deleteUnauthorizedTest(ApiTester $I)
+    {
+        $I->amUser('user');
+
+        $I->sendDELETE('/v1/survey/1');
+        $I->canSeeResponseCodeIs(403);
+    }
+
+    public function deleteTest(ApiTester $I)
+    {
+        $I->amStaff();
+
+        $I->sendDELETE('/v1/survey/1');
+        $I->canSeeResponseCodeIs(201);
+
+        $I->seeInDatabase('survey', ['id' => 1, 'status' => -1]);
+    }
+}
