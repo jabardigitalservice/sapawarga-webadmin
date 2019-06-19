d0583401a16d9fccd6c3132878ed881707c324d2
Ma'ruf <maruf.harsono@gmail.com>
2019-06-19 17:04:46 +0700

    admin-polling-list status

--- backend/src/views/polling/list.vue
+++ backend/src/views/polling/list.vue
@@ -99,9 +99,51 @@ export default {
         this.listLoading = false
       })
     },
+    getStatusColor(row) {
+      const now = moment()
+      const startDate = moment(row.start_date).startOf('day')
+      const endDate = moment(row.end_date).endOf('day')
+
+      const isRunning = now.isBetween(startDate, endDate, null, '[]')
+
+      if (row.status === 10 && isRunning) {
+        return 'success'
+      }
+
+      if (row.status === 10 && now.isAfter(endDate)) {
+        return 'warning'
+      }
+
+      if (row.status === 1) {
+        return 'danger'
+      }
+
+      if (row.status === 0) {
+        return 'info'
+      }
+
+      return row.status_label
+    },
+    getStatusLabel(row) {
+      const now = moment()
+      const startDate = moment(row.start_date).startOf('day')
+      const endDate = moment(row.end_date).endOf('day')
+
+      const isRunning = now.isBetween(startDate, endDate, null, '[]')
+
+      if (row.status === 10 && isRunning) {
+        return 'Sedang Berlangsung'
+      }
+
+      if (row.status === 10 && now.isAfter(endDate)) {
+        return 'Berakhir'
+      }
+
+      return row.status_label
+    },
     getTableRowNumbering(index) {
       return ((this.listQuery.page - 1) * this.listQuery.limit) + (index + 1)
-    }
+    },
     changeSort(e) {
       this.listQuery.sort_by = e.prop
       this.listQuery.sort_order = e.order
