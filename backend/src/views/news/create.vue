0a241ab3bb02c0121967aefe1ce64c74ef9b0c78
Ma'ruf <maruf.harsono@gmail.com>
2019-06-26 14:46:57 +0700

    admin-news create

--- backend/src/views/news/create.vue
+++ backend/src/views/news/create.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Create news</h1>
+  <div class="app-container">
+    <p class="warn-content"><a href="#">Tambah Berita</a></p>
+    <Form />
+  </div>
 </template>
+
+<script>
+import Form from './components/FormNews'
+
+export default {
+  components: { Form }
+}
+</script>
