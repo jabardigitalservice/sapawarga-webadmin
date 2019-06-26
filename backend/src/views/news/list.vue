b8004568e2ffe8fd2bfce69f2bd9f6ed269bf468
Ganjar Setia <ganjar_setia@ymail.com>
2019-06-26 17:02:05 +0700

    WIP: unit test list and list news

--- backend/src/views/news/list.vue
+++ backend/src/views/news/list.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>List News</h1>
+  <div class="app-container">
+    <el-row :gutter="10">
+      <el-col class="col-left" :xs="24" :sm="24" :md="24" :lg="10" :xl="10">
+        <el-card>
+          <div slot="header" class="clearfix">
+            <span>Kanal Media</span>
+          </div>
+          <el-table stripe :data="tableDataStatistik" :show-header="false" style="width: 100%">
+            <el-table-column prop="title" />
+            <el-table-column prop="content" />
+          </el-table>
+        </el-card>
+      </el-col>
+      <!-- <el-col class="col-right" :xs="24" :sm="24" :md="24" :lg="14" :xl="14">
+        <el-card>
+          <div slot="header" class="clearfix">
+            <span>Isi Pesan</span>
+          </div>
+          <el-table stripe :data="tableDataPesan" :show-header="false" style="width: 100%">
+            <el-table-column prop="title" width="180" />
+            <el-table-column prop="content" />
+          </el-table>
+        </el-card>
+        <el-button v-if="!btnKirimDisable" class="button-send" type="primary" @click="actionApprove(status.active)">{{ $t('crud.send') }}</el-button>
+      </el-col> -->
+    </el-row>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableDataStatistik: []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@media only screen and (max-width: 1200px) {
+  .col-right {
+    margin-top: 20px;
+    margin-right: 20px !important
+  }
+}
+</style>
