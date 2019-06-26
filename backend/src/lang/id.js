00fd300e8d005fe3c8cbe04f9b4f9d2d32de4e6b
Ma'ruf <maruf.harsono@gmail.com>
2019-06-26 17:04:07 +0700

    admin-news added save-news in lang id.js

--- backend/src/lang/id.js
+++ backend/src/lang/id.js
@@ -2,6 +2,7 @@ export default {
   route: {
     dashboard: 'Dashboard',
     'profile': 'Profile',
+
     'user-manage': 'Kelola Pengguna',
     'user-create': 'Tambah Pengguna Baru',
     'user-list-provinsi': 'Provinsi',
@@ -9,34 +10,41 @@ export default {
     'user-list-kecamatan': 'Kecamatan',
     'user-list-kelurahan': 'Kelurahan',
     'user-list-rw': 'User Tingkat RW',
+
     'nomor-penting-manage': 'Kelola Nomor Penting',
     'nomor-penting-list': 'Daftar Nomor Penting',
     'nomor-penting-create': 'Tambah Nomor Penting',
     'nomor-penting-edit': 'Edit Nomor Penting',
     'nomor-penting-detail': 'Detail Nomor Penting',
+
     'broadcast-manage': 'Broadcast',
     'broadcast-create': 'Tambah Pesan',
     'broadcast-list': 'Daftar Broadcast',
     'broadcast-edit': 'Edit Broadcast',
     'broadcast-show': 'Detail Broadcast',
+
     'notification-manage': 'Notification',
     'notification-create': 'Tambah Pesan',
     'notification-list': 'Daftar Notification',
     'notification-edit': 'Edit Notification',
     'notification-show': 'Detail Notification',
+
     'aspiration-manage': 'Aspirasi Masyarakat',
     'aspiration-list': 'Aspirasi Masyarakat',
     'aspiration-detail': 'Detail Aspirasi',
+
     'polling-manage': 'Polling',
     'polling-list': 'Daftar Polling',
     'polling-create': 'Tambah Polling',
     'polling-detail': 'Detail Polling',
     'polling-edit': 'Edit Polling',
+
     'survey-manage': 'Survey',
     'survey-list': 'Daftar Survey',
     'survey-create': 'Tambah Survey',
     'survey-detail': 'Detail Survey',
     'survey-edit': 'Edit Survey',
+
     'news-manage': 'Berita',
     'news-list': 'Daftar Berita',
     'news-create': 'Tambah Berita',
@@ -46,20 +54,22 @@ export default {
   },
   crud: {
     'list-empty': 'Belum ada data.',
-
     'create': 'Buat Baru',
-    'save-create': 'Simpan',
     'update': 'Edit',
-    'save-update': 'Simpan Perubahan',
     'delete': 'Hapus',
     'cancel': 'Batal',
     'back': 'Kembali',
-    'back-to-list': 'Kembali ke Daftar',
-    'draft': 'Simpan sebagai Draft',
-    'save-publish': 'Simpan dan Aktifkan',
-    'send': 'Kirim Pesan',
     'accept': 'Terima',
     'reject': 'Tolak',
+    'send': 'Kirim Pesan',
+    'draft': 'Simpan sebagai Draft',
+
+    'save-create': 'Simpan',
+    'save-update': 'Simpan Perubahan',
+    'save-publish': 'Simpan dan Aktifkan',
+    'save-news': 'Tambah Berita',
+
+    'back-to-list': 'Kembali ke Daftar',
     'send-polling': 'Publikasikan Polling',
     'publish-polling': 'Polling berhasil dipublikasikan',
 
@@ -74,8 +84,10 @@ export default {
     'update-success': 'Perubahan data berhasil disimpan.',
     'send-success': 'Pesan berhasil dikirim',
     'draft-success': 'Pesan berhasil disimpan di draft',
+
     'send-polling-success': 'Polling berhasil dipublikasikan',
     'draft-polling-success': 'Polling berasil disimpan di draft',
+
     'send-notification-success': 'Notifikasi berhasil di kirim',
     'draft-notification-success': 'Notifikasi berhasil di simpan di draft',
 
@@ -88,6 +100,7 @@ export default {
     'deactivate-confirm': 'Anda yakin ingin menonaktifkan data ini?',
     'activate-success': 'Data berhasil diaktifkan.',
     'deactivate-success': 'Data berhasil dinonaktifkan.',
+
     'polling-error-edit-published': 'Sudah dipublish, tidak dapat diedit.',
     'broadcast-error-edit-published': 'Sudah dipublish, tidak dapat di edit.',
     'notification-error-edit-published': 'Sudah dipublish, tidak dapat di edit.'
