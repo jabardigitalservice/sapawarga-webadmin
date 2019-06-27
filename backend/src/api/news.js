eb0e5df66d151e9ec3291e8abb10dc1593d886c1
Ganjar Setia <ganjar_setia@ymail.com>
2019-06-27 15:03:35 +0700

    create API for news

---
+++ backend/src/api/news.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+export function fetchList(query) {
+  return request({
+    url: '/news',
+    method: 'get',
+    params: query
+  })
+}
+
+export function fetchRecord(id) {
+  return request({
+    url: `/news/${id}`,
+    method: 'get'
+  })
+}
+
+export function create(data) {
+  return request({
+    url: '/news',
+    method: 'post',
+    data
+  })
+}
+
+export function update(id, data) {
+  return request({
+    url: `/news/${id}`,
+    method: 'put',
+    data
+  })
+}
+
+export function approval(id, data) {
+  return request({
+    url: `/news/approval/${id}`,
+    method: 'post',
+    data
+  })
+}
