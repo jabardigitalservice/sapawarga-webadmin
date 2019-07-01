10ff1f0122cbe79c00f821fe1e32b1659dd167a8
Aditya Satrya <aditya.satrya@gmail.com>
2019-07-01 14:34:28 +0700

    remove frontend folder

--- frontend/src/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = process.env.VUE_APP_API_URL
-export const ACCESS_TOKEN_PREFIX = process.env.VUE_APP_ACCESS_TOKEN_PREFIX
-export const SITE_TITLE = 'Frontend App'
--- frontend/src/lib/components/chart/types/bubble/index.vue
+++
@@ -1,6 +0,0 @@
-<script>
-import makeChart from '../../base'
-import { Bubble } from 'vue-chartjs'
-
-export default makeChart(Bubble)
-</script>
