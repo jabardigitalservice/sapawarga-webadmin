208b2eb653fb0159d060c1dc7f664cf919f3d873
Aditya Satrya <aditya.satrya@gmail.com>
2019-07-01 14:34:16 +0700

    remove api folder

--- api/config/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = require(__DIR__ . '/db.php');
-// test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=' . getenv('MYSQL_HOST')
-    . ';port=' . getenv('MYSQL_PORT')
-    . ';dbname=' . getenv('MYSQL_DATABASE');
-
-return $db;
