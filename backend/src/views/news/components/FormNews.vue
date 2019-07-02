3d3706153e07e903c7934e4bb4d6fa5e44a97311
Ma'ruf <maruf.harsono@gmail.com>
2019-07-02 16:16:35 +0700

    admin-news edit change varible name on upload photo

--- backend/src/views/news/components/FormNews.vue
+++ backend/src/views/news/components/FormNews.vue
@@ -2,7 +2,7 @@
   <div class="components-container">
     <el-row :gutter="20">
       <el-col :xs="24" :sm="8" :lg="5">
-        <AttachmentPhotoUpload type="news_photo" :initial-url="news.cover_url" style="margin-bottom: 25px" @onUpload="photoUploaded" />
+        <AttachmentPhotoUpload type="news_photo" :initial-url="news.cover_path_url" style="margin-bottom: 25px" @onUpload="photoUploaded" />
       </el-col>
       <el-col :xs="24" :sm="16" :lg="19">
         <el-alert
