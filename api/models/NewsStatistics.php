9bb36d8e608bdbc50cc24cff412de488299a7c6a
aldidoanta <aldidkurnia@gmail.com>
2019-07-03 15:20:44 +0700

    [API][News] Create logic to show news statistics, sortable

--- api/models/NewsStatistics.php
+++ api/models/NewsStatistics.php
@@ -3,7 +3,7 @@
 namespace app\models;
 
 use Illuminate\Support\Arr;
-use yii\data\ActiveDataProvider;
+use yii\data\SqlDataProvider;
 
 /**
  * NewsSearch represents the model behind the search form of `app\models\News`.
@@ -15,22 +15,35 @@ class NewsStatistics extends News
      *
      * @param array $params
      *
-     * @return ActiveDataProvider
+     * @return SqlDataProvider
      */
 
     public function getStatistics($params)
     {
-        // \yii\helpers\VarDumper::dump('asddada');
-        $query = NewsChannel::find()->with('news.channel_id');
-        $query->andFilterWhere(['status' => NewsChannel::STATUS_ACTIVE]);
+        $sql = 'SELECT news_channels.id, news_channels.name as title, COALESCE(stat.news_count, 0) as count
+                FROM news_channels
+                LEFT JOIN (
+                    SELECT news.channel_id, COUNT(news.channel_id) as news_count
+                    FROM news
+                    GROUP BY news.channel_id
+                ) as stat
+                ON news_channels.id = stat.channel_id';
 
-        $sortBy    = Arr::get($params, 'sort_by', 'name');
+        $sortBy    = Arr::get($params, 'sort_by', 'title');
         $sortOrder = Arr::get($params, 'sort_order', 'ascending');
         $sortOrder = $this->getSortOrder($sortOrder);
 
-        return new ActiveDataProvider([
-            'query'      => $query,
-            'sort'       => ['defaultOrder' => [$sortBy => $sortOrder]],
+        return new SqlDataProvider([
+            'sql'      => $sql,
+            'sort'     => [
+                'defaultOrder'  => [$sortBy => $sortOrder],
+                'attributes'    => [
+                    'id',
+                    'title',
+                    'name',
+                    'count',
+                ],
+            ],
         ]);
     }
 
