aed3dd70c0d60fd2b003a49e637a8ae6059da01d
Ganjar Setia <ganjar_setia@ymail.com>
2019-07-16 14:49:06 +0700

    try fix build & add artifact

--- backend/image-files/usr/local/bin/docker-entrypoint.sh
+++ backend/image-files/usr/local/bin/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 if [ "$NODE_ENV" = 'development' ]; then
     cd /app && yarn && yarn run dev
 else
+    cd /app && yarn && yarn build
     printf "Starting nginx...\n\n"
     nginx -g "daemon off;"
 fi
