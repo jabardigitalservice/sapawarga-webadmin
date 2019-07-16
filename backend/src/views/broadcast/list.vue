3619d8b8e47c96a6ccf8aa234ba9ebb8b8317d33
maruf12 <maruf.harsono@gmail.com>
2019-07-16 17:33:00 +0700

    admin-broadcast-roles check roles in add and edit button

--- backend/src/views/broadcast/list.vue
+++ backend/src/views/broadcast/list.vue
@@ -8,7 +8,7 @@
         <el-row style="margin: 10px 0px">
           <el-col :span="12">
             <router-link :to="{ path: '/broadcast/create' }">
-              <el-button type="primary" size="small" icon="el-icon-plus">
+              <el-button v-if="roles" type="primary" size="small" icon="el-icon-plus">
                 Tambah Pesan Baru
               </el-button>
             </router-link>
@@ -50,7 +50,7 @@
                 </el-button>
               </router-link>
               <router-link :to="(scope.row.status !== 10 ? '/broadcast/edit/' +scope.row.id : '')">
-                <el-button type="white" size="mini" :disabled="scope.row.status === 10">
+                <el-button v-if="roles" type="white" size="mini" :disabled="scope.row.status === 10">
                   Edit
                 </el-button>
               </router-link>
