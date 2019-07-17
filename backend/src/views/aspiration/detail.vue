5f64556f1a7f77ed9faf44bc08a2da4c5a0e7353
maruf12 <maruf.harsono@gmail.com>
2019-07-17 16:06:35 +0700

    admin-role aspiration added roles

--- backend/src/views/aspiration/detail.vue
+++ backend/src/views/aspiration/detail.vue
@@ -66,7 +66,7 @@ export default {
       images: [],
       index: null,
       note: null,
-      disableButton: false
+      roles: checkPermission(['admin', 'staffProv'])
     }
   },
   computed: {
