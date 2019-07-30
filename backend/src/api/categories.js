9842ed5fd236ab9e4b83c023c89ae04e5fb78a60
Ganjar Setia <ganjar_setia@ymail.com>
2019-07-30 12:06:20 +0700

    add for create edit and API for category

--- backend/src/api/categories.js
+++ backend/src/api/categories.js
@@ -8,8 +8,50 @@ export function fetchList(query) {
   })
 }
 
+export function fetchRecord(id) {
+  return request({
+    url: `/categories/${id}`,
+    method: 'get'
+  })
+}
+
+export function fetchTypes() {
+  return request({
+    url: `/categories/types`,
+    method: 'get'
+  })
+}
+
+export function create(data) {
+  return request({
+    url: '/categories',
+    method: 'post',
+    data
+  })
+}
+
+export function update(id, data) {
+  return request({
+    url: `/categories/${id}`,
+    method: 'put',
+    data
+  })
+}
+
+export function deleteData(id) {
+  return request({
+    url: `/categories/${id}`,
+    method: 'delete'
+  })
+}
+
 const exportFunctions = {
-  fetchList
+  fetchList,
+  fetchRecord,
+  fetchTypes,
+  create,
+  update,
+  deleteData
 }
 
 export default exportFunctions
