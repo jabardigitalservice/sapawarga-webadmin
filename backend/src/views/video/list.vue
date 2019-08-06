a6e681076f5635f97a05c7219f2123e77499b94e
maruf12 <maruf.harsono@gmail.com>
2019-08-06 10:36:21 +0700

    admin-video-detailAndSort added changeSort in list video

--- backend/src/views/video/list.vue
+++ backend/src/views/video/list.vue
@@ -30,19 +30,19 @@
             </router-link>
           </el-col>
         </el-row>
-
+        
         <ListFilter :list-query.sync="listQuery" @submit-search="getList" @reset-search="resetFilter" />
 
-        <el-table v-loading="listLoading" :data="list" border stripe fit highlight-current-row style="width: 100%">
+        <el-table v-loading="listLoading" :data="list" border stripe fit highlight-current-row @sort-change="changeSort" style="width: 100%">
           <el-table-column type="index" align="center" width="50" :index="getTableRowNumbering" />
 
-          <el-table-column label="Judul Video" sortable="custome" prop="title" min-width="200">
+          <el-table-column label="Judul Video" sortable="custom" prop="title" min-width="200">
             <template slot-scope="{row}">
               {{ text_truncate(row.title) }}
             </template>
           </el-table-column>
 
-          <el-table-column prop="category.name" sortable="custome" label="Kategori" align="center" width="120" />
+          <el-table-column prop="category.name" sortable="custom" label="Kategori" align="center" width="120" />
 
           <el-table-column
             prop="status"
