96eab0e0eb688bde200d3ead891cfd3c8fc193e1
maruf12 <maruf.harsono@gmail.com>
2019-08-15 17:21:03 +0700

    admin-dashboard-template added category componet

---
+++ backend/src/views/dashboard/admin/components/Category.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-table
+    :data="tableData"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      prop="usulan"
+      label="Kategori Popular"
+      align="center"
+      min-width="200"
+      >
+    </el-table-column>
+    <el-table-column
+      prop="category"
+      label="Usulan"
+      align="center"
+      min-width="100"
+      >
+    </el-table-column>
+  </el-table>
+</template>
