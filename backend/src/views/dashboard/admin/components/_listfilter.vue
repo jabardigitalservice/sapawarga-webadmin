7ed507a1de937f96756c87136cf860ead9742701
maruf12 <maruf.harsono@gmail.com>
2019-08-19 14:37:27 +0700

    admin-dashboard-usulan added filter

---
+++ backend/src/views/dashboard/admin/components/_listfilter.vue
@@ -0,0 +1,88 @@
+<template>
+  <el-card class="box-card" style="margin-bottom: 10px">
+    <el-form>
+      <el-row :gutter="10" type="flex">
+        <el-col :span="9">
+          <el-form-item style="margin-bottom: 0">
+            <InputCategory v-model="listQuery.category_id" name="category_id" category-type="aspirasi" prop="category" style="width: 100%" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="9">
+          <el-form-item style="margin-bottom: 0">
+            <el-select v-model="listQuery.kabkota_id" filterable clearable placeholder="Kota/Kabupaten">
+              <el-option
+                v-for="item in area"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-button type="primary" size="small" @click="submitSearch">
+            Cari
+          </el-button>
+          <el-button type="primary" size="small" @click="resetFilter">
+            Reset
+          </el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+import InputCategory from '@/components/InputCategory'
+
+import { getKabkotaList } from '@/api/areas'
+
+export default {
+  components: {
+    InputCategory
+  },
+  props: {
+    listQuery: {
+      type: Object,
+      default: null
+    },
+    status: {
+      type: String,
+      default: null
+    }
+  },
+
+  data() {
+    return {
+      area: null
+    }
+  },
+
+  created() {
+    this.getArea()
+  },
+
+  methods: {
+
+    changeKabkota(id) {
+      this.listQuery.kabkota_id = id
+    },
+
+    submitSearch() {
+      this.$emit('submit-search')
+    },
+
+    resetFilter() {
+      this.$emit('reset-search')
+    },
+
+    getArea() {
+      getKabkotaList().then(response => {
+        this.area = response.data.items
+        console.log(this.area)
+      })
+    },
+
+  }
+}
+</script>
