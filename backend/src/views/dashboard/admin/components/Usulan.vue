845f3bbc258b45fc0edd203c5dbc53633272c9ef
maruf12 <maruf.harsono@gmail.com>
2019-08-19 13:35:18 +0700

    admin-dashboard-usulan get list usulan

--- backend/src/views/dashboard/admin/components/Usulan.vue
+++ backend/src/views/dashboard/admin/components/Usulan.vue
@@ -1,23 +1,62 @@
 <template>
-  <el-table
-    :data="tableData"
-    stripe
-    style="width: 100%"
-  >
+  <el-table class="title" v-loading="listLoading" :data="list" stripe border highlight-current-row style="width: 100%">
     <el-table-column
-      prop="usulan"
+      prop="title"
       label="Usulan"
-      align="center"
+      align="left"
+      min-width="180"
+      style="background:blue"
     />
     <el-table-column
-      prop="category"
+      prop="category_name"
       label="Kategori"
       align="center"
     />
     <el-table-column
-      prop="like"
+      prop="total_likes"
       label="Likes"
       align="center"
     />
   </el-table>
 </template>
+
+<script>
+import { fetchList } from '@/api/dashboard'
+
+export default {
+  components: {},
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      listQuery: {
+        category_id: null,
+        kabkota_id: null
+      }
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.items
+        console.log(this.list)
+        this.listLoading = false
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  // el-table-column:first-child {
+  //   margin-left: 50px;
+  // }
+  .title {
+    padding: 0 0px;
+  }
+
+</style>
