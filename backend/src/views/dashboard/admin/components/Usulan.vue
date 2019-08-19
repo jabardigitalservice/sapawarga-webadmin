878873db661710e19b71479dc46323501ba935d6
maruf12 <maruf.harsono@gmail.com>
2019-08-19 14:39:18 +0700

    admin-dashboard-usulan refactor table

--- backend/src/views/dashboard/admin/components/Usulan.vue
+++ backend/src/views/dashboard/admin/components/Usulan.vue
@@ -2,25 +2,26 @@
   <el-card class="box-card">
     <ListFilter :list-query.sync="listQuery" @submit-search="getList" @reset-search="resetFilter" />
 
-  <el-table class="title" v-loading="listLoading" :data="list" stripe border highlight-current-row style="width: 100%">
-    <el-table-column
-      prop="title"
-      label="Usulan"
-      align="left"
-      min-width="180"
-      style="background:blue"
-    />
-    <el-table-column
-      prop="category_name"
-      label="Kategori"
-      align="center"
-    />
-    <el-table-column
-      prop="total_likes"
-      label="Likes"
-      align="center"
-    />
-  </el-table>
+    <el-table class="title" v-loading="listLoading" :data="list" stripe border highlight-current-row style="width: 100%">
+      <el-table-column
+        prop="title"
+        label="Usulan"
+        align="left"
+        min-width="180"
+        style="background:blue"
+      />
+      <el-table-column
+        prop="category_name"
+        label="Kategori"
+        align="center"
+      />
+      <el-table-column
+        prop="total_likes"
+        label="Likes"
+        align="center"
+      />
+    </el-table>
+  </el-card>
 </template>
 
 <script>
