65d33de8726a7ffeda525ec4e3f65c793e6fd2df
maruf12 <maruf.harsono@gmail.com>
2019-08-21 13:35:45 +0700

    admin-dashboard lint fix

--- backend/tests/unit/Dashboard.spec.js
+++ backend/tests/unit/Dashboard.spec.js
@@ -1,28 +1,28 @@
-import { shallowMount, createLocalVue, mount } from "@vue/test-utils";
-import DashboardUsulan from "@/views/dashboard/admin/components/Usulan";
-import DashboardApproval from "@/views/dashboard/admin/components/Approval";
-import ListFilter from "@/views/dashboard/admin/components/_listfilter";
-import ElementUI from "element-ui";
+import { shallowMount, createLocalVue } from "@vue/test-utils"
+import DashboardUsulan from "@/views/dashboard/admin/components/Usulan"
+import DashboardApproval from "@/views/dashboard/admin/components/Approval"
+import ListFilter from "@/views/dashboard/admin/components/_listfilter"
+import ElementUI from "element-ui"
 
-const localVue = createLocalVue();
-localVue.use(ElementUI);
+const localVue = createLocalVue()
+localVue.use(ElementUI)
 
 beforeEach(() => {
-  jest.resetModules();
-  jest.clearAllMocks();
+  jest.resetModules()
+  jest.clearAllMocks()
 });
 
 describe("List dashboard usulan", () => {
   it("render list usulan", () => {
     const wrapper = shallowMount(DashboardUsulan, {
       localVue
-    });
+    })
 
     wrapper.vm.getList()
     wrapper.vm.resetFilter()
 
     expect(wrapper.contains(ListFilter)).toBe(true)
-  });
+  })
 
   it("render _listfilter", () => {
     const listQuery = {
@@ -41,16 +41,16 @@ describe("List dashboard usulan", () => {
     wrapper.vm.resetFilter()
     wrapper.vm.getArea()
     
-    expect(wrapper.props("listQuery")).toBe(listQuery);
-    expect(wrapper.emitted()['submit-search']).toBeTruthy();
-    expect(wrapper.emitted()['reset-search']).toBeTruthy();
-  });
+    expect(wrapper.props("listQuery")).toBe(listQuery)
+    expect(wrapper.emitted()['submit-search']).toBeTruthy()
+    expect(wrapper.emitted()['reset-search']).toBeTruthy()
+  })
 
   it("render dashboard approval", () => {
     const wrapper = shallowMount(DashboardApproval, {
       localVue
-    });
+    })
 
     expect(wrapper.is(DashboardApproval)).toBe(true)
   })
-});
+})
