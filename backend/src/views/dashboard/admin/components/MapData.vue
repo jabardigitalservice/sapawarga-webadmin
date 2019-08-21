287e3b6a6117b8a70b7d179b8a35579a471d1a65
maruf12 <maruf.harsono@gmail.com>
2019-08-21 17:33:18 +0700

    admin-dashboard-map template

--- backend/src/views/dashboard/admin/components/MapData.vue
+++ backend/src/views/dashboard/admin/components/MapData.vue
@@ -3,26 +3,23 @@
     <div class="text item">
       <el-row :gutter="8">
         <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 16}" :xl="{span: 16}" style="padding-right:8px;margin-bottom:30px;">
-          <h1 align="center">Map Jawa Barat</h1>
+          <div class="mapouter">
+            <div class="gmap_canvas">
+              <iframe
+                id="gmap_canvas"
+                :src="`https://maps.google.com/maps?q=${latitude},${longitude}&t=&z=16&ie=UTF8&iwloc=&output=embed`"
+                frameborder="0"
+                scrolling="no"
+                marginheight="0"
+                marginwidth="0"
+              />
+            </div>
+          </div>
         </el-col>
         <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 8}" :xl="{span: 8}" style="margin-bottom:30px;">
-          <el-table
-
-            stripe
-            style="width: 100%; margin-left:10px"
-          >
-            <el-table-column
-              prop="usulan"
-              label="Kota/Kabupaten"
-              align="center"
-              min-width="200"
-            />
-            <el-table-column
-              prop="category"
-              label="Usulan"
-              align="center"
-              min-width="100"
-            />
+          <el-table :data="list" stripe style="width: 100%; margin-left:10px">
+            <el-table-column prop="name" label="Kota/Kabupaten" align="left" min-width="200" />
+            <el-table-column prop="counts" label="Usulan" align="center" min-width="100" />
           </el-table>
         </el-col>
       </el-row>
