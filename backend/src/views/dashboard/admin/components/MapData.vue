bdee0ffaf72c780a03beb64e09b1eacb80741e53
a.rohman <aldirohman@live.com>
2019-08-27 16:45:27 +0700

    fix error linter 'google is not defined'

--- backend/src/views/dashboard/admin/components/MapData.vue
+++ backend/src/views/dashboard/admin/components/MapData.vue
@@ -3,8 +3,7 @@
     <div class="text item">
       <el-row :gutter="8">
         <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 16}" :xl="{span: 16}" style="padding-right:8px;margin-bottom:30px;">
-            <div class="gmap_canvas" id="gmaps">
-          </div>
+          <div id="gmaps" class="gmap_canvas" />
         </el-col>
         <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 8}" :xl="{span: 8}" style="margin-bottom:30px;">
           <el-table :data="list" stripe style="width: 100%; margin-left:10px">
@@ -21,26 +20,15 @@
 import { fetchAspirasiMap } from '@/api/dashboard'
 import gmapsInit from '@/utils/gmaps'
 export default {
-    async mounted() {
-    try {
-      const google = await gmapsInit()
-      const element = document.getElementById('gmaps')
-      const options = {
-      zoom: 8,
-      center: new google.maps.LatLng(-6.943097,107.633545)
-    }
-      this.map = new google.maps.Map(element, options)
-      
-    } catch (error) {
-      console.error(error);
-      this.$message.error(this.$t('dashboard-map-error'))
-    }
-  },
   data() {
     return {
       list: null,
       map: null,
-      activeInfoWindow: null
+      activeInfoWindow: null,
+      jawaBarat: {
+        lat: -6.943097,
+        lon: 107.633545
+      }
     }
   },
   created() {
@@ -50,31 +38,43 @@ export default {
     getMap() {
       fetchAspirasiMap().then(response => {
         this.list = response.data.items
-        this.createMarker()
+        this.createMap(this.list)
       })
     },
-    createMarker() {
-      this.list.forEach((coord) => {
-        const position = new google.maps.LatLng(coord.longitude, coord.latitude)
-          const marker = new google.maps.Marker({ 
+    async createMap(dataMap) {
+      try {
+        const google = await gmapsInit()
+        const element = document.getElementById('gmaps')
+        const options = {
+          zoom: 8,
+          center: new google.maps.LatLng(this.jawaBarat.lat, this.jawaBarat.lon)
+        }
+        this.map = new google.maps.Map(element, options)
+
+        dataMap.forEach((coord) => {
+          const position = new google.maps.LatLng(coord.longitude, coord.latitude)
+          const marker = new google.maps.Marker({
             position: position,
             map: this.map
-        })
+          })
 
-        const infoWindow = new google.maps.InfoWindow({
-          content: `<div>${coord.name}</div><br>
+          const infoWindow = new google.maps.InfoWindow({
+            content: `<div>${coord.name}</div><br>
                     <div style="text-align: center">${coord.counts} Usulan</div>`
-        })
+          })
 
-        marker.addListener('click', () => {
-          if (this.activeInfoWindow) {
-            this.activeInfoWindow.close()
-          }          
-          infoWindow.open(this.map, marker)
-          this.activeInfoWindow = infoWindow
-
+          marker.addListener('click', () => {
+            if (this.activeInfoWindow) {
+              this.activeInfoWindow.close()
+            }
+            infoWindow.open(this.map, marker)
+            this.activeInfoWindow = infoWindow
+          })
         })
-      })
+      } catch (error) {
+        console.error(error)
+        this.$message.error(this.$t('dashboard-map-error'))
+      }
     }
   }
 }
