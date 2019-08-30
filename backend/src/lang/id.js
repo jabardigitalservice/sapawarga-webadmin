28dff809397eca0e10018fe1a97993b71883b5cc
maruf12 <maruf.harsono@gmail.com>
2019-08-30 17:34:15 +0700

    admin-news-role refactor lang id.js

--- backend/src/lang/id.js
+++ backend/src/lang/id.js
@@ -131,7 +131,7 @@ export default {
     'broadcast-error-edit-published': 'Sudah dipublish, tidak dapat di edit.',
     'notification-error-edit-published': 'Sudah dipublish, tidak dapat di edit.',
     'video-error-edit-published': 'Sudah dipublish, tidak dapat di edit.',
-    'video-error-edit-role': 'Anda tidak memiliki hak akses halaman ini.'
+    'error-edit-role': 'Anda tidak memiliki hak akses halaman ini.'
   },
   common: {
     confirm: 'Ya',
