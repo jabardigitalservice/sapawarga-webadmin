1f882a52a4819c2600b61733bec8572c858a388c
maruf12 <maruf.harsono@gmail.com>
2019-08-30 17:36:33 +0700

    admin-news-roles added condition disabled button

--- backend/src/views/news/list.vue
+++ backend/src/views/news/list.vue
@@ -3,7 +3,6 @@
     <el-row :gutter="10">
       <el-col class="col-left" :xs="24" :sm="24" :md="24" :lg="7" :xl="7">
         <el-card>
-          {{ user_id }}
           <div slot="header" class="clearfix">
             <span>Kanal Media</span>
           </div>
@@ -81,19 +80,19 @@
                   <el-button type="primary" icon="el-icon-view" size="small" />
                 </el-tooltip>
               </router-link>
-              <router-link :to="(scope.row.status !== 10 ? '/news/edit/' +scope.row.id : '')">
+              <router-link :to="(scope.row.status !== 10 && scope.row.created_by === user_id ? '/news/edit/' +scope.row.id : '')">
                 <el-tooltip content="Edit Berita" placement="top">
-                  <el-button type="warning" icon="el-icon-edit" size="small" :disabled="scope.row.status === 10" />
+                  <el-button type="warning" icon="el-icon-edit" size="small" :disabled="scope.row.status === 10 || scope.row.created_by !== user_id" />
                 </el-tooltip>
               </router-link>
               <el-tooltip content="Hapus Berita" placement="top">
-                <el-button type="danger" icon="el-icon-delete" size="small" :disabled="scope.row.status === 10" @click="deleteNews(scope.row.id)" />
+                <el-button type="danger" icon="el-icon-delete" size="small" :disabled="scope.row.status === 10 || scope.row.created_by !== user_id" @click="deleteNews(scope.row.id)" />
               </el-tooltip>
               <el-tooltip content="Nonaktifkan Berita" placement="top">
-                <el-button v-if="scope.row.status === 10" type="danger" icon="el-icon-circle-close" size="small" @click="deactivateRecord(scope.row.id)" />
+                <el-button v-if="scope.row.status === 10" type="danger" icon="el-icon-circle-close" size="small" :disabled="scope.row.created_by !== user_id" @click="deactivateRecord(scope.row.id)" />
               </el-tooltip>
               <el-tooltip content="Aktifkan Berita" placement="top">
-                <el-button v-if="scope.row.status === 0" type="success" icon="el-icon-circle-check" size="small" @click="activateRecord(scope.row.id)" />
+                <el-button v-if="scope.row.status === 0" type="success" icon="el-icon-circle-check" size="small" :disabled="scope.row.created_by !== user_id" @click="activateRecord(scope.row.id)" />
               </el-tooltip>
             </template>
           </el-table-column>
@@ -145,15 +144,16 @@ export default {
       tableDataStatistikTotal: [{ title: 'Semua Kategori', count: 0 }]
     }
   },
-  created() {
-    this.getList()
-    this.getStatistic()
-  },
 
   computed: {
     ...mapGetters([
       'user_id'
     ])
+  },
+
+  created() {
+    this.getList()
+    this.getStatistic()
   },
 
   methods: {
