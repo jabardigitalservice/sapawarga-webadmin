af60765ef779c5e317da5de58f27b2239ad83081
Aditya Satrya <aditya.satrya@gmail.com>
2019-09-11 12:38:44 +0700

    rebuild app when running container to adjust env variables

--- backend/image-files/usr/local/bin/docker-entrypoint.sh
+++ backend/image-files/usr/local/bin/docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 if [ "$NODE_ENV" = 'development' ]; then
-    printf "Build and run server for dev...\n\n"
+    printf "Build and run server for development...\n\n"
     cd /app && yarn && yarn run dev
+    printf "Build and run server finished\n\n"
 else
+    printf "Rebuild for prodution...\n\n"
+    cd /app && yarn && yarn build
+    printf "Build finished\n\n"
+
     printf "Starting nginx...\n\n"
     nginx -g "daemon off;"
+    printf "Nginx started\n\n"
 fi
