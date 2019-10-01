863ad37d3cec7aad1fae4af2f7c1cc202854c410
maruf12 <maruf.harsono@gmail.com>
2019-10-01 16:58:37 +0700

    admin-banner list filter

---
+++ backend/src/views/banner/_listfilter.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-card class="box-card" style="margin-bottom: 10px">
+    <el-form>
+      <el-row :gutter="10" type="flex">
+        <el-col :span="(isPriority)? 12:18">
+          <el-form-item style="margin-bottom: 0">
+            <el-input v-model="listQuery.search" placeholder="Judul" />
+          </el-form-item>
+        </el-col>
+        <el-col v-if="isPriority" :span="6">
+          <el-form-item style="margin-bottom: 0">
+            <el-select
+              v-model="listQuery.channel_id"
+              clearable
+              filterable
+              placeholder="Pilih Sumber Berita"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="item in options"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-button type="primary" size="small" @click="submitSearch">Cari</el-button>
+          <el-button type="primary" size="small" @click="resetFilter">Reset</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+import checkPermission from '@/utils/permission'
+import { newsChannelList } from '@/api/news'
+export default {
+  props: {
+    listQuery: {
+      type: Object,
+      default: null
+    },
+    isPriority: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      options: []
+    }
+  },
+  created() {
+    this.getNewsChannel()
+  },
+  methods: {
+    checkPermission,
+
+    submitSearch() {
+      this.$emit('submit-search')
+    },
+
+    resetFilter() {
+      this.$emit('reset-search')
+    },
+
+    getNewsChannel() {
+      newsChannelList().then(response => {
+        this.options = response.data.items
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.mt-10 {
+  margin-top: 10px;
+}
+</style>
