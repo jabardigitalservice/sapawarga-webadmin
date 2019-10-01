422600910f5ed73e6eac5ac71c5e4e8b81beea51
maruf12 <maruf.harsono@gmail.com>
2019-10-01 16:59:32 +0700

    admin-banner formbanner

---
+++ backend/src/views/banner/components/FormBanner.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="components-container">
+    <el-row :gutter="20">
+      <el-col :xs="24" :sm="8" :lg="5">
+        <AttachmentPhotoUpload type="news_photo" :initial-url="banner.cover_path_url" style="margin-bottom: 25px" @onUpload="photoUploaded" />
+      </el-col>
+      <el-col :xs="24" :sm="16" :lg="19"> 
+        <el-form ref="banner" :model="banner" :rules="rules" :status-icon="true" label-width="160px">
+          <el-form-item label="Judul Banner" prop="title">
+            <el-input v-model="banner.title" type="text" name="title" placeholder="Judul Banner" />
+          </el-form-item>
+          <el-form-item label="Tipe" prop="type">
+            <el-radio-group v-model="banner.type">
+              <el-radio label="eksternal">Eksternal</el-radio>
+              <el-radio label="internal">Internal</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item v-if="banner.type === 'eksternal'" label="Tautan" prop="type">
+            <el-input v-model="banner.internal_category" type="text" name="title" placeholder="URL Banner" />
+          </el-form-item>
+          <el-form-item v-else label="Kategori" prop="category">
+             <el-select v-model="banner.internal_entity_id" placeholder="Pilih Kategori">
+               <el-option label="Survei" value="survei"></el-option>
+                <el-option label="Polling" value="polling"></el-option>
+                <el-option label="Berita" value="berita"></el-option>
+             </el-select>
+            <!-- <span v-if="banner.internal_entity_id !== null">
+              <el-button>Pilihan</el-button>
+            </span> -->
+          </el-form-item>
+          <el-form-item v-if="banner.internal_entity_id !== null">
+            <el-button @click="dialogTableVisible = true">Pilihan</el-button>
+          </el-form-item>
+          <el-form-item label="Status" prop="status">
+            <el-tooltip :content="banner.status == 0 ? 'nonaktif' : 'aktif'" placement="top">
+              <el-switch
+                v-model="banner.status"
+                active-color="#13ce66"
+                inactive-color="#ff4949"
+                active-value="10"
+                inactive-value="0">
+              </el-switch>
+            </el-tooltip>
+          </el-form-item> {{banner.status}}
+          <el-form-item>
+            <el-button v-if="isEdit" type="primary" :loading="loading" @click="submitForm">{{ $t('crud.save-update') }}</el-button>
+            <el-button v-else type="primary" :loading="loading" @click="submitForm">{{ $t('crud.save-banner') }}</el-button>
+
+            <router-link :to="'/banner/index'">
+              <el-button type="info">{{ $t('crud.cancel') }}</el-button>
+            </router-link>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <el-dialog v-el-drag-dialog :visible.sync="dialogTableVisible" title="List Berita">
+      <el-row>
+        <ListFilter
+          :list-query.sync="listQuery"
+          :is-priority="true"
+          @submit-search="getListBerita"
+          @reset-search="resetFilter"
+        />
+        <!-- <el-table :data="listBerita">
+          <el-table-column type="index" width="50" align="center" :index="getTableRowNumbering" />
+          <el-table-column property="title" label="Judul Berita" />
+          <el-table-column property="channel.name" label="Sumber" />
+          <el-table-column property="kabkota.name" label="Target Berita" />
+          <el-table-column align="center" label="Actions">
+            <template slot-scope="scope">
+              <el-button type="white" size="mini" @click="addBeritaPriority(scope.row), dialogTableVisible = false">
+                Tambah
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <pagination
+          v-show="total>0"
+          :total="total"
+          :page.sync="listQuery.page"
+          :limit.sync="listQuery.limit"
+          @pagination="getListBerita"
+        /> -->
+      </el-row>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import AttachmentPhotoUpload from '@/components/AttachmentPhotoUpload'
+import Pagination from '@/components/Pagination'
+import ListFilter from './_listfilter'
+export default {
+  components: { 
+    AttachmentPhotoUpload,
+    Pagination,
+    ListFilter, 
+  },
+  props: {
+    isEdit: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      loading: false,
+      banner: {
+        title: null,
+        internal_category: null,
+        internal_entity_id: null,
+        cover_path: null,
+        cover_path_url: null,
+        status: 0,
+        type: "eksternal"
+      },
+      dialogTableVisible: false,
+      rules: {
+        title: [
+          {
+            required: true,
+            message: 'Judul Banner harus diisi',
+            trigger: 'blur'
+          },
+        ],
+        type: [
+          {
+            required: true,
+            message: 'Tipe Banner harus diisi',
+            trigger: 'blur'
+          },
+        ],
+        category: [
+          {
+            required: true,
+            message: 'Kategori Banner harus diisi',
+            trigger: 'blur'
+          }
+        ],
+        status: [
+          {
+            required: true,
+            message: 'Status harus diisi',
+            trigger: 'blur'
+          },
+        ]
+      }
+    }
+  },
+  methods: {
+    photoUploaded(path, url) {
+      this.banner.cover_path = path
+    },
+    async submitForm() {
+
+    }
+  }
+}
+</script>
