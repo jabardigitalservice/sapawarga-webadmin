f8ab4d944aea805cc3ae39bd38f5c89220119f1e
arifboyz16 <arifboyz16@gmail.com>
2019-10-17 10:41:45 +0700

    styling image top berita

--- backend/src/views/dashboard/admin/components/TopBerita.vue
+++ backend/src/views/dashboard/admin/components/TopBerita.vue
@@ -1,17 +1,26 @@
 <template>
   <el-card class="box-card">
     <el-table v-loading="listLoading" class="title" :data="list" stripe border highlight-current-row style="width: 100%">
-      <!--       <el-table-column min-width="200">
-        <div slot-scope="{row}" class="img-container">
-          <img :src="source_url">
-        </div>
-      </el-table-column> -->
       <el-table-column
         prop="title"
         :label="judul"
         align="left"
         style="background:blue"
-      />
+        valign="middle"
+      >
+        <template slot-scope="{row}">
+          <el-row>
+            <el-col :span="3">
+              <img :src="row.cover_path_url" style="width: 28px;border-radius: 50%;">
+            </el-col>
+            <el-col :span="10">
+              <div class="title-top-berita cell" style="using display:table-cell; vertical-align:middle;padding-left: 10px;width: 200px;">
+                <span>{{ row.title }}</span>
+              </div>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
       <el-table-column
         prop="total_viewers"
         label="View"
