5b209e8526da1ec0dca2fba82c7f3b9e38e1a7c2
maruf12 <maruf.harsono@gmail.com>
2019-10-29 13:09:22 +0700

    admin-broadcast fix unit test

--- backend/tests/unit/Broadcast.spec.js
+++ backend/tests/unit/Broadcast.spec.js
@@ -2,6 +2,9 @@ import { shallowMount, createLocalVue, shallow, mount } from "@vue/test-utils";
 import ElementUI from "element-ui";
 import BroadcastList from "@/views/broadcast/list";
 import ListFilter from "@/views/broadcast/_listfilter";
+import broadcastListFixture from './fixtures/newsList';
+import * as apiBroadcast from '@/api/broadcast'
+import flushPromises from 'flush-promises'
 
 const localVue = createLocalVue();
 localVue.use(ElementUI);
@@ -12,18 +15,27 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-describe("News List", () => {
-  it("renders channel statistic", () => {
-    const channel = [
-      { title: "humas jabar", content: 50 },
-      { title: "kompas", content: 20 },
-      { title: "Tempo", content: 10 }
-    ];
+jest.mock('@/api/broadcast')
+apiBroadcast.fetchList = () => Promise.resolve(broadcastListFixture)
 
+describe("News List", () => {
+  const expectedList = broadcastListFixture.data.items
+  it("renders channel statistic", async () => {
+    
     const wrapper = shallowMount(BroadcastList, {
-      localVue
-    });
-    expect(wrapper.contains(ListFilter)).toBe(true);
+      localVue,
+      mocks: {
+        $t: () => {}
+      }
+    })
+
+    await flushPromises()
+
+    expect(wrapper.vm.list).toBe(expectedList)
+    expect(wrapper.contains(ListFilter)).toBe(true)
+    wrapper.vm.getTableRowNumbering()
+    wrapper.vm.resetFilter()
+    expect(wrapper.vm.list).toBe(expectedList)
   });
 
   it("renders _listfilter", () => {
@@ -38,7 +50,7 @@ describe("News List", () => {
       kec_id: null,
       kel_id: null
     }
-    
+
     const wrapper = shallowMount(ListFilter, {
       localVue,
       propsData: {
@@ -49,10 +61,11 @@ describe("News List", () => {
           state: {
             user: {"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NjM0NjU4NzksImlzcyI6Imh0dHA6XC9cLzM1LjI0Ny4xMzUuOTMueGlwLmlvOjgxIiwiYXVkIjoiaHR0cDpcL1wvMzUuMjQ3LjEzNS45My54aXAuaW86ODEiLCJuYmYiOjE1NjM0NjU4NzksImV4cCI6MTU2MzU1MjI3OSwiZGF0YSI6eyJ1c2VybmFtZSI6InN0YWZma2Fia290YSIsInJvbGVMYWJlbCI6IlN0YWYgS2FidXBhdGVuXC9Lb3RhIiwibGFzdExvZ2luQXQiOnsiZXhwcmVzc2lvbiI6IlVOSVhfVElNRVNUQU1QKCkiLCJwYXJhbXMiOltdfX0sImp0aSI6M30.BYTUR7Nt8m2D02-5-sPuU3ALqRgbWC-oU8pxCOdDenU","name":"Staff Kota Bandung","avatar":"/static/img/user.91347baa.png","introduction":"","roles":["staffKabkota"],"roles_active":{"id":"staffKabkota","label":"Staf Kabupaten/Kota"},"kel_id":null,"kec_id":null,"kabkota_id":22,"kabkota":"KOTA BANDUNG","kecamatan":null,"kelurahan":null,"dummy_image":"/static/img/user.91347baa.png"}
           }
-        }
+        },
+        $t: () => {}
       }
-    });
-
+    })
+      
     wrapper.vm.submitSearch()
     wrapper.vm.resetFilter()
     wrapper.vm.changeKabkota(1)
