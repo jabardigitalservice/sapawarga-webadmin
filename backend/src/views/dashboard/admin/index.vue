bc4e3168f010c11fdcfd69aaf877737217bd657e
maruf12 <maruf.harsono@gmail.com>
2019-10-30 16:02:30 +0700

    admin-newsImportant lint fix

--- backend/src/views/dashboard/admin/index.vue
+++ backend/src/views/dashboard/admin/index.vue
@@ -7,7 +7,7 @@
       <line-chart :chart-data="lineChartData" />
     </el-row> -->
 
-    <h3 style="color:#73737">{{$t('dashboard.dashboard-aspiration')}}</h3>
+    <h3 style="color:#73737">{{ $t('dashboard.dashboard-aspiration') }}</h3>
 
     <el-row :gutter="8">
       <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 16}" :xl="{span: 16}" style="padding-right:8px;margin-bottom:10px;">
@@ -27,7 +27,7 @@
     </el-row>
 
     <!-- Polling -->
-    <h3 style="color:#73737">{{$t('dashboard.dashboard-polling')}}</h3>
+    <h3 style="color:#73737">{{ $t('dashboard.dashboard-polling') }}</h3>
     <el-row>
       <el-col>
         <polling />
@@ -35,7 +35,7 @@
     </el-row>
 
     <!-- Polling -->
-    <h3 style="color:#73737">{{$t('dashboard.dashboard-top-news')}}</h3>
+    <h3 style="color:#73737">{{ $t('dashboard.dashboard-top-news') }}</h3>
     <el-row>
       <el-col :xs="{span: 12}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 12}" :xl="{span: 12}">
         <TopNews :list="listNewsProvinsi" :title="$t('dashboard.dashboard-news-prov')" />
