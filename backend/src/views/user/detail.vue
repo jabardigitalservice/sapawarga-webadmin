9806332f521157ee5844ef5e69ebad4e51bff568
arifboyz16 <arifboyz16@gmail.com>
2019-11-05 23:02:23 +0700

    format tanggal detail user

--- backend/src/views/user/detail.vue
+++ backend/src/views/user/detail.vue
@@ -81,7 +81,6 @@ import MapThumb from '@/components/MapThumb'
 import { fetchUser } from '@/api/staff'
 import permission from '@/directive/permission/index.js'
 import { parsingDatetime, formatDatetime } from '@/utils/datetimeToString'
-import moment from 'moment'
 
 export default {
   components: { PhotoBox, MapThumb },
@@ -173,7 +172,7 @@ export default {
           },
           {
             title: this.$t('label.birthdate'),
-            content: formatDatetime(birth_date, "DD MMMM YYYY") || '-'
+            content: formatDatetime(birth_date, 'DD MMMM YYYY') || '-'
           },
           {
             title: this.$t('label.education'),
@@ -244,7 +243,7 @@ export default {
           },
           {
             title: this.$t('label.last-access-at'),
-            content: last_access_at ? formatDatetime(last_access_at, "DD MMMM YYYY HH:mm") : '-'
+            content: last_access_at ? formatDatetime(last_access_at, 'DD MMMM YYYY HH:mm') : '-'
           }
         ]
       })
