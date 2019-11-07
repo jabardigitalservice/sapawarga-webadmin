683539873a6c4e7f3f851c8ca6a69d06c921d519
maruf12 <maruf.harsono@gmail.com>
2019-11-07 17:45:33 +0700

    admin-newsImportant detail

--- backend/src/views/newsImportant/detail.vue
+++ backend/src/views/newsImportant/detail.vue
@@ -20,22 +20,52 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        tableDataRecord: []
-      }
-    },
-    async created() {
-      const id = await this.$route.params && this.$route.params.id
-      await this.getDetail(id)
-    },
-    methods: {
-      getDetail(id) {
-        fetchRecord(id).then(response => {
-          const {title, category, } = response.data
-        })
-      }
+import { fetchRecord } from '@/api/newsImportant'
+export default {
+  data () {
+    return {
+      tableDataRecord: []
     }
+  },
+  async created() {
+    const id = await this.$route.params && this.$route.params.id
+    await this.getDetail(id)
+  },
+  methods: {
+    getDetail(id) {
+      fetchRecord(id).then(response => {
+        const {title, category, source_url, content} = response.data
+        console.log(response.data)
+
+        this.tableDataRecord = [
+          {
+            title: 'Judul',
+            content: title
+          },
+          {
+            title: 'Kategori',
+            content: category.name
+          },
+          {
+            title: 'Gambar',
+            content: 'Kucing'
+          },
+          {
+            title: 'Tautan',
+            content: source_url
+          },
+          {
+            title: 'Deskripsi',
+            content: this.strip(content)
+          }
+        ]
+      })
+    },
+    strip(html) {
+      var tmp = document.createElement('DIV')
+      tmp.innerHTML = html
+      return tmp.textContent || tmp.innerText || ''
+    },
   }
+}
 </script>
