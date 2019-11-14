bdc4eac39f5c434b4d0ddfdaf464d0f4ebedc42f
maruf12 <maruf.harsono@gmail.com>
2019-11-14 16:51:53 +0700

    admin-newsImportant detail

--- backend/src/views/newsImportant/detail.vue
+++ backend/src/views/newsImportant/detail.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="app-container">
-    <p class="warn-content"><a href="#">{{$t('label.newsImportant-detail')}}</a></p>
+    <p class="warn-content"><a href="#">{{ $t('label.newsImportant-detail') }}</a></p>
 
     <el-row>
       <el-col :sm="24">
         <el-card>
           <div slot="header" class="clearfix">
-            <span>{{$t('label.newsImportant-data')}}</span>
+            <span>{{ $t('label.newsImportant-data') }}</span>
           </div>
           <el-table stripe :data="tableDataRecord" :show-header="false" style="width: 100%">
             <el-table-column prop="title" width="180" />
             <el-table-column prop="content">
               <template slot-scope="{row}">
-                <a v-if="validUrl(row.content)" :href="row.content" target="_blank" class="link">{{ row.content }}</a>
-                <span v-else>{{ row.content }}</span>
+                <a v-if="validUrl(row.content)" :href="row.content" target="_blank" class="link-news-important">{{ row.content }}</a>
+                <span v-else v-html="row.content" />
+              </template>
+            </el-table-column>
+          </el-table>
+          <el-table stripe :data="tableImageAttachment" :show-header="false" style="width: 100%">
+            <el-table-column prop="title" width="180" />
+            <el-table-column prop="content">
+              <img :src="image || imageNone" alt="" width="300" height="225">
+            </el-table-column>
+          </el-table>
+          <el-table v-if="check" stripe :data="attachments" :show-header="false" style="width: 100%">
+            <el-table-column prop="title" width="180" />
+            <el-table-column prop="content">
+              <template slot-scope="{row}">
+                <ul v-for="(data, index) in row.content" :key="index">
+                  <li><a :href="data.file_url" target="_blank" class="link-news-important">{{ data.name }}</a><br></li>
+                </ul>
               </template>
             </el-table-column>
           </el-table>
         </el-card>
-
       </el-col>
     </el-row>
   </div>
@@ -26,12 +41,18 @@
 
 <script>
 import { fetchRecord } from '@/api/newsImportant'
-import { render, validUrl } from '@/utils/htmlRender'
-// import { validUrl } from '@/utils/validate'
+import { validUrl } from '@/utils/htmlRender'
+
 export default {
-  data () {
+  data() {
     return {
-      tableDataRecord: []
+      tableDataRecord: [],
+      tableImageAttachment: [],
+      attachments: [],
+      image: null,
+      imageNone: require('@/assets/none.png'),
+      check: null,
+      lampiran: null
     }
   },
   async created() {
@@ -39,46 +60,50 @@ export default {
     await this.getDetail(id)
   },
   methods: {
+    validUrl,
     getDetail(id) {
       fetchRecord(id).then(response => {
-        const {title, category, source_url, content} = response.data
-        console.log(response.data)
+        const { title, category, source_url, content, image_path_url, attachments } = response.data
+        this.image = response.data.image_path_url
+        this.check = response.data.attachments
+
+        function logArrayAnswers(value, index, array) {
+          return value
+        }
 
         this.tableDataRecord = [
           {
-            title: 'Judul',
+            title: this.$t('label.newsImportant-title'),
             content: title
           },
           {
-            title: 'Kategori',
+            title: this.$t('label.category'),
             content: category.name
           },
           {
-            title: 'Gambar',
-            content: 'Kucing'
-          },
-          {
-            title: 'Tautan',
+            title: this.$t('label.link'),
             content: source_url
           },
           {
-            title: 'Deskripsi',
-            content: render(content)
-          }, 
+            title: this.$t('label.newsImportant-description'),
+            content: content
+          }
+        ]
+
+        this.tableImageAttachment = [
           {
-            
+            title: this.$t('label.image'),
+            content: image_path_url
+          }
+        ]
+
+        this.attachments = [
+          {
+            title: this.$t('label.attachment'),
+            content: attachments ? attachments.map(logArrayAnswers) : null
           }
         ]
       })
-    },
-    validUrl(str) {
-      var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i') // fragment locator
-      return !!pattern.test(str)
     }
   }
 }
