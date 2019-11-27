eea12b1b3742390250cfc6cc421916d5f8ee02f1
arifboyz16 <arifboyz16@gmail.com>
2019-11-27 19:57:50 +0700

    user list

--- backend/src/views/questionsAnswer/components/userList.vue
+++ backend/src/views/questionsAnswer/components/userList.vue
@@ -15,7 +15,7 @@
 
         <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="1">
           <div
-            v-for="(user, key) in data"
+            v-for="user in data"
             :key="user.id"
             class="users-containers"
             @click="onSelectUser(user)"
@@ -77,7 +77,7 @@ export default {
       const response = await fetchListUser(this.listQuery)
       this.data = []
       this.data.push(...response.data.items)
-      
+
     },
     async 'listQuery.kabkota_id'() {
       const response = await fetchListUser(this.listQuery)
