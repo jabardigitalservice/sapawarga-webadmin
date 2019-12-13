868fe0aa8771007eacebfd8c87cd5da09376af90
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-12-13 10:19:01 +0700

    Add several feature to DetailData component to be able to show image, url, and html element, Add component style

--- backend/src/components/DetailData/index.vue
+++ backend/src/components/DetailData/index.vue
@@ -3,8 +3,10 @@
     <el-row v-for="(element, index) in tableContentData" :key="index" :gutter="10">
       <el-col :xs="24" :sm="6">{{ element.title }} {{ device==='mobile'?':':'' }}</el-col>
       <el-col :xs="24" :sm="18">
-        <a v-if="validUrl(element.content)" :href="element.content" target="_blank" class="link">{{ element.content }}</a>
-        <span v-else>{{ element.content }}</span>
+        <div v-if="inputImage"><img :src="inputImage" alt=""></div>
+        <div v-else-if="validUrl(element.content)"><a :href="element.content" target="_blank" class="link">{{ element.content }}</a></div>
+        <div v-else-if="isContainHtmlTags(element.content)" v-html="element.content" />
+        <div v-else><span>{{ element.content }}</span></div>
       </el-col>
     </el-row>
   </div>
@@ -12,6 +14,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { isContainHtmlTags, validUrl } from '@/utils/validate'
 
 export default {
   name: 'DetailData',
@@ -20,6 +23,10 @@ export default {
       type: Array,
       default: null,
       require: true
+    },
+    inputImage: {
+      type: String,
+      default : null
     }
   },
   computed: {
@@ -28,25 +35,36 @@ export default {
     ])
   },
   methods: {
-    validUrl(str) {
-      var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i') // fragment locator
-      return !!pattern.test(str)
-    }
+    isContainHtmlTags,
+    validUrl
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .el-row {
+  display: flex;
+  align-items: center;
   margin-bottom: 10px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #e6ebf5;
 
-  &:last-child {
-      margin-bottom: 0;
+  & img {
+    width: 100%;
+    height: auto;
+    max-width: 300px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .el-row { 
+    display: block;
+
+    & .el-col {
+      &:first-child {
+        margin-bottom: 10px;
+      }
+    }  
   }
 }
 </style>
