73e8662837aa3c520782ffc1419abaa5a0f2bd6f
maruf12 <maruf.harsono@gmail.com>
2019-12-16 12:57:57 +0700

    admin-post-list added post router

---
+++ backend/src/router/modules/post.js
@@ -0,0 +1,37 @@
+import Layout from '@/layout'
+
+const postRouter = {
+  name: 'Post',
+  path: '/post',
+  component: Layout,
+  redirect: '/post/index',
+  meta: {
+    title: 'post-manage',
+    roles: ['admin', 'staffProv'],
+    icon: 'example'
+  },
+  children: [
+    {
+      name: 'post-list',
+      path: 'index',
+      component: () => import('@/views/post/list'),
+      meta: {
+        title: 'post-list',
+        roles: ['admin', 'staffProv']
+      }
+    },
+
+    {
+      name: 'post-detail',
+      path: 'detail/:id',
+      component: () => import('@/views/post/detail'),
+      hidden: true,
+      meta: {
+        title: 'post-detail',
+        roles: ['admin', 'staffProv']
+      }
+    }
+  ]
+}
+
+export default postRouter
