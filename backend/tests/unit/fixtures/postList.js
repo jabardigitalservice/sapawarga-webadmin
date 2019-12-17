b5487dd932f06765ef6a9cb932724d24f48441f9
maruf12 <maruf.harsono@gmail.com>
2019-12-17 17:56:29 +0700

    admin-post test unit fixture

---
+++ backend/tests/unit/fixtures/postList.js
@@ -0,0 +1,112 @@
+export default {
+	status: 200,
+	success: true,
+	data: {
+		items: [
+			{
+        "id": 1,
+        "title": "Tolong perbaikan jalan berlubang",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "kabkota_id": 22,
+        "kabkota": {
+          "id": 22,
+          "name": "Kota Bandung"
+        },
+        "kec_id": 446,
+        "kecamatan": {
+          "id": 446,
+          "name": "Bandung Wetan"
+        },
+        "kel_id": 6082,
+        "kelurahan": {
+          "id": 6082,
+          "name": "Cicaheum"
+        },
+        "status": 10,
+        "status_label": "Dipublikasikan",
+        "likes_counts": 10,
+        "author_id": 3,
+        "author": {
+          "id": 1,
+          "name": "Muhammad Rahmat Irfan"
+        },
+        "category_id": 1,
+        "category": {
+          "id": 1,
+          "name": "Infrastruktur"
+        },
+        "images": [
+          {
+            "url": "https://dummyimage.com/wsxga"
+          },
+          {
+            "url": "https://dummyimage.com/wsxga"
+          },
+          {
+            "url": "https://dummyimage.com/wsxga"
+          },
+          {
+            "url": "https://dummyimage.com/wsxga"
+          }
+        ],
+        "created_at": 1554076800,
+        "updated_at": 1554076800
+			},
+			{
+        "id": 2,
+        "title": "Pungli pada penerimaan siswa baru SMA Negeri Maju Jaya",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "kabkota_id": 22,
+        "kabkota": {
+          "id": 22,
+          "name": "Kota Bandung"
+        },
+        "kec_id": 446,
+        "kecamatan": {
+          "id": 446,
+          "name": "Bandung Wetan"
+        },
+        "kel_id": 6082,
+        "kelurahan": {
+          "id": 6082,
+          "name": "Cicaheum"
+        },
+        "status": 10,
+        "status_label": "Dipublikasikan",
+        "likes_counts": 10,
+        "author_id": 3,
+        "author": {
+          "id": 1,
+          "name": "Muhammad Rahmat Irfan"
+        },
+        "category_id": 1,
+        "category": {
+          "id": 1,
+          "name": "Infrastruktur"
+        },
+        "images": [
+          {
+            "url": "https://dummyimage.com/wsxga"
+          },
+          {
+            "url": "https://dummyimage.com/wsxga"
+          },
+          {
+            "url": "https://dummyimage.com/wsxga"
+          },
+          {
+            "url": "https://dummyimage.com/wsxga"
+          }
+        ],
+        "created_at": 1554076800,
+        "updated_at": 1554076800
+      }
+		],
+		"_meta": {
+      "totalCount": 100,
+      "pageCount": 5,
+      "currentPage": 1,
+      "perPage": 20
+    }
+	}
+}
