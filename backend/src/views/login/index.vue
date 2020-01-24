f1e31126db060dd652a9aceadc83b69ae3c23725
maruf12 <maruf.harsono@gmail.com>
2020-01-24 17:35:38 +0700

    admin-login added enviroment varible phone number

--- backend/src/views/login/index.vue
+++ backend/src/views/login/index.vue
@@ -53,7 +53,7 @@
         style="width:100%"
         @click.native.prevent="handleLogin"
       >{{ $t('label.login') }}</el-button>
-      <h4>{{ $t('label.login-forgot-password') }} <span class="contact-admin" @click="onClickChat('082315192724')">{{ $t('label.login-forgot-password-number') }}</span></h4>
+      <h4>{{ $t('label.login-forgot-password') }} <span class="contact-admin" @click="onClickChat(phone)">{{ $t('label.login-forgot-password-number') }}</span></h4>
     </el-form>
   </div>
 </template>
@@ -95,7 +95,8 @@ export default {
       passwordType: 'password',
       loading: false,
       showDialog: false,
-      redirect: undefined
+      redirect: undefined,
+      phone: process.env.VUE_APP_PHONE
     }
   },
   watch: {
@@ -152,6 +153,7 @@ export default {
     },
 
     onClickChat(phone) {
+      console.log(phone)
       const url_link = 'https://wa.me/' + phone.replace(/^0+/, '62')
       window.open(url_link, '_blank')
     }
