9ab3051c9dc8b05c2eb9d5359cd2f098686accd5
maruf12 <maruf.harsono@gmail.com>
2020-01-30 09:03:36 +0700

    admin-popup added unit test popup

---
+++ backend/tests/unit/fixtures/popupList.js
@@ -0,0 +1,52 @@
+export default {
+	status: 200,
+	success: true,
+	data: {
+		items: [
+      {
+				id: 33,
+				title: "Ini apa yes?",
+				description: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
+				image_path: "general/1579891815-X6lvFKQUI3vrjvTKVjx9jc4tf9MBeEYh.jpg",
+				image_path_url: "https://d2o6nohz2yihhc.cloudfront.net/general/1579891815-X6lvFKQUI3vrjvTKVjx9jc4tf9MBeEYh.jpg",
+				type: "internal",
+				link_url: "",
+				internal_object_type: "news",
+				internal_object_id: 100,
+				internal_object_name: "Jabar meraih penghargaan baru",
+				status: 10,
+				status_label: "Aktif",
+				start_date: "2020-01-25 01:48:45",
+				end_date: "2020-01-31 01:48:45",
+				created_at: 1579891841,
+				updated_at: 1580201658,
+				created_by: 1
+			},
+			{
+				id: 31,
+				title: "Panduan Komunitas Sapawarga",
+				description: "Sapawarga memiliki Fitur Usulan yang dapat digunakan oleh Masyarakat Jawa Barat untuk memberikan usulan, ide, dan aspirasi. Usulan, ide, dan aspirasi yang diberikan sesuai dan tidak bertentangan dengan koridor hukum yang berlaku di Negara Kesatuan Republik Indonesia (NKRI).",
+				image_path: "general/1576477682-ovq2hoLYzgcOCmYHtFwzjN5KcaCXqDRI.jpg",
+				image_path_url: "https://d2o6nohz2yihhc.cloudfront.net/general/1576477682-ovq2hoLYzgcOCmYHtFwzjN5KcaCXqDRI.jpg",
+				type: "external",
+				link_url: "https://digitalservice.jabarprov.go.id/index.php/panduan-komunitas-sapawarga/",
+				internal_object_type: null,
+				internal_object_id: null,
+				internal_object_name: "",
+				status: 10,
+				status_label: "Aktif",
+				start_date: "2019-12-16 13:28:14",
+				end_date: "2019-12-23 11:33:47",
+				created_at: 1576471426,
+				updated_at: 1576658643,
+				created_by: 186201
+			}
+		],
+		"_meta": {
+      "totalCount": 100,
+      "pageCount": 5,
+      "currentPage": 1,
+      "perPage": 20
+    }
+	}
+}
---
+++ backend/tests/unit/popup.spec.js
@@ -0,0 +1,89 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Router from 'vue-router'
+import ElementUI from 'element-ui'
+import flushPromises from 'flush-promises'
+import * as api from '@/api/popupInformasi'
+import ListFilter from '@/views/popupInformasi/_listfilter'
+import popupListFixture from './fixtures/popupList'
+import PopupList from '@/views/popupInformasi/list'
+import PopupDetail from '@/views/popupInformasi/detail'
+
+const router = new Router()
+const localVue = createLocalVue()
+localVue.use(Router)
+localVue.use(ElementUI)
+
+
+beforeEach(() => {
+	jest.resetModules()
+	jest.clearAllMocks()
+})
+
+jest.mock('@/api/popupInformasi')
+api.fetchList = () => Promise.resolve(popupListFixture)
+
+const listQuery = {
+	type: null,
+	title: null,
+	status: null
+}
+
+describe('Popup', () => {
+	const expectedPopupList = popupListFixture.data.items
+	it('list popup', async() => {
+		const status = {
+			'10': 'success',
+			'0': 'info',
+			'-1': 'danger',
+			'5': 'warning'
+		}
+		const wrapper = shallowMount(PopupList, {
+			localVue,
+			mocks: {
+				$t: () => {}
+			},
+			propsData: {
+				listQuery
+			}
+		})
+
+		await flushPromises()
+
+		expect(wrapper.contains(PopupList)).toBe(true)
+		wrapper.vm.resetFilter()
+		wrapper.vm.getTableRowNumbering()
+		wrapper.vm.changeSort('title')
+		expect(wrapper.vm.list).toBe(expectedPopupList)
+	})
+
+	it('filter', () => {
+		const wrapper = shallowMount(ListFilter, {
+			localVue,
+			mocks: {
+				$t: () => {}
+			},
+			propsData: {
+				listQuery
+			}
+		})
+
+		wrapper.vm.submitSearch()
+		wrapper.vm.resetFilter()
+
+		expect(wrapper.props('listQuery')).toBe(listQuery)
+    expect(wrapper.emitted()['submit-search']).toBeTruthy()
+    expect(wrapper.emitted()['reset-search']).toBeTruthy()
+	})
+
+	it('detail', () => {
+		const wrapper = shallowMount(PopupDetail, {
+			localVue,
+			router,
+			mocks: {
+				$t: () => {}
+			}
+		})
+		wrapper.vm.getDetail()
+		expect(wrapper.contains(PopupDetail)).toBe(true)
+	})
+})
