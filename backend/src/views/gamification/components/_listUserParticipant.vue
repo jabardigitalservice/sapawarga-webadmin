a7104bf949b113eea256cd2f76718c1f7bb3a212
arifboyz16 <arifboyz16@gmail.com>
2020-02-04 11:19:25 +0700

    list user participant gamification

---
+++ backend/src/views/gamification/components/_listUserParticipant.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-table
+    :data="listParticipant"
+    style="width: 100%"
+  >
+    <el-table-column
+      type="index"
+      width="50"
+      align="center"
+      :index="getTableRowNumbering"
+    />
+    <el-table-column
+      prop="name"
+      label="Nama"
+      width="180"
+    />
+    <el-table-column
+      prop="address"
+      label="Kedudukan"
+    />
+    <el-table-column
+      prop="task"
+      label="Task"
+      width="180"
+    />
+    <el-table-column
+      prop="date"
+      label="Tanggal Bergabung"
+      width="180"
+    />
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'UserParticipant',
+  props: {
+    listParticipant: {
+      type: Array,
+      default: null,
+      require: true
+    }
+  },
+  methods: {
+    getTableRowNumbering(index) {
+      return (index + 1)
+    }
+  }
+}
+</script>
