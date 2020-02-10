ee370f36926bf4c48fc9216a6f4a5e53b03caf17
maruf12 <maruf.harsono@gmail.com>
2020-02-10 11:26:43 +0700

    admin-opd-permission added staffOPD in survey

--- backend/src/views/survey/list.vue
+++ backend/src/views/survey/list.vue
@@ -79,7 +79,7 @@ export default {
       total: 0,
       listLoading: true,
       RolesUser,
-      roles: checkPermission([RolesUser.ADMIN, RolesUser.STAFFPROV, RolesUser.LEADER]),
+      roles: checkPermission([RolesUser.ADMIN, RolesUser.STAFFPROV, RolesUser.LEADER, RolesUser.STAFFOPD]),
       listQuery: {
         title: null,
         status: null,
