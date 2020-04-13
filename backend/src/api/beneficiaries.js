76071f6c18d97c478f61bf2745abdb7a88722c5b
maruf <maruf.harsono@gmail.com>
2020-04-13 15:59:48 +0700

    added api beneficiaries

---
+++ backend/src/api/beneficiaries.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+
+export function fetchList(query) {
+  return request({
+    url: '/beneficiaries',
+    method: 'get',
+    params: query
+  })
+}
+
+export function fetchRecord(id) {
+  return request({
+    url: `/beneficiaries/${id}`,
+    method: 'get'
+  })
+}
+
+export function create(data) {
+  return request({
+    url: '/beneficiaries',
+    method: 'post',
+    data
+  })
+}
+
+export function update(id, data) {
+  return request({
+    url: `/beneficiaries/${id}`,
+    method: 'put',
+    data
+  })
+}
