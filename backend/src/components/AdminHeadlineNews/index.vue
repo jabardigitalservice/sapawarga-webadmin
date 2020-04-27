a7a96bf8a63d20aa36b7a4d69744b42b0dbae219
Yoga Hanggara <yohang88@gmail.com>
2020-04-27 23:39:26 +0700

    banner
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- backend/src/components/AdminHeadlineNews/index.vue
+++ backend/src/components/AdminHeadlineNews/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <el-row v-if="user.roles_active.id === 'staffKabkota'">
+      <a :href="link" target="_blank">
+        <img src="https://firebasestorage.googleapis.com/v0/b/sapawarga-staging.appspot.com/o/admin-banner-01.png?alt=media&token=3ad9f723-a397-48c4-9c8e-41e6cfbf5d6c" width="100%" />
+      </a>
+    </el-row>
     <el-row v-if="user.roles_active.id === 'staffKabkota'" id="headline-news">
       <el-card class="box-card">
         <div slot="header">
