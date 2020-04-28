82ebff7d65f80876d588958051b9c80f1e14ec2d
Yoga Hanggara <yohang88@gmail.com>
2020-04-28 20:49:20 +0700

    page bansos
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- backend/src/views/bansos/upload.vue
+++ backend/src/views/bansos/upload.vue
@@ -1,6 +1,51 @@
 <template>
   <div class="app-container">
-    <div>Upload</div>
+    <el-row>
+      <el-col :span="24">
+        <el-card class="box-card">
+          <div slot="header">
+            <span>Upload Data KRTS Non DTKS</span>
+          </div>
+          <div class="text item">
+            <p><strong>Pilih pintu bantuan untuk upload KRTS</strong></p>
+            <p>Sebelum melakukan upload Data KRTD Non DTKS, diharapkan data dipecah berdasarkan alokasi sesuai pintu bantuan. Untuk melakukan upload data, pilih pintu bantuan yang akan di upload.</p>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <router-link to="/bansos/upload">
+          <el-card class="box-card">
+            <p>Bantuan Sosial Provinsi</p>
+          </el-card>
+        </router-link>
+
+        <router-link to="/bansos/upload">
+          <el-card class="box-card">
+            <p>Bantuan Sosial Kota / Kabupaten</p>
+          </el-card>
+        </router-link>
+
+        <router-link to="/bansos/upload">
+          <el-card class="box-card">
+            <p>Dana Desa</p>
+          </el-card>
+        </router-link>
+
+        <router-link to="/bansos/upload">
+          <el-card class="box-card">
+            <p>Bantuan Sosial Presiden Sembako (Bodebek)</p>
+          </el-card>
+        </router-link>
+
+        <router-link to="/bansos/upload">
+          <el-card class="box-card">
+            <p>Bantuan Sosial Tunai Kemensos</p>
+          </el-card>
+        </router-link>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
