51ecdb93a2d61d2fbf0925efb23f0709aa154242
Irfan Kamil <kmz.in.act@gmail.com>
2020-05-05 14:46:14 +0700

    dashboard bansos

--- backend/src/api/beneficiaries.js
+++ backend/src/api/beneficiaries.js
@@ -8,6 +8,22 @@ export function fetchSummary(query) {
   })
 }
 
+export function fetchDashboardSummary(query) {
+  return request({
+    url: '/pub/beneficiaries/dashboard-summary',
+    method: 'get',
+    params: query
+  })
+}
+
+export function fetchDashboardList(query) {
+  return request({
+    url: '/pub/beneficiaries/dashboard-list',
+    method: 'get',
+    params: query
+  })
+}
+
 export function fetchList(query) {
   return request({
     url: '/beneficiaries',
