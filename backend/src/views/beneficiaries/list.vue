ef2c4cd86c1275367345c00d1a3788759e299081
maruf <maruf.harsono@gmail.com>
2020-05-12 14:03:14 +0700

    dialog opening

--- backend/src/views/beneficiaries/list.vue
+++ backend/src/views/beneficiaries/list.vue
@@ -2,6 +2,10 @@
   <div class="app-container">
     <el-row>
       <el-col :lg="24">
+        <div class="warn-content-warning">
+          <p class="title">Pengumuman</p>
+          <p>Proses verifikasi dan validasi (verval) data penerima bansos via Sapawarga PERIODE 2 kembali dibuka mulai Senin, 11 Mei 2020! <span class="link" @click="dialogVisible = true">Lihat Info Selengkapnya</span></p>
+        </div>
         <el-row style="margin: 10px 0px">
           <el-col :span="12">
             <el-button type="primary" size="small" icon="el-icon-plus" @click="accessBlock('create')">
@@ -77,16 +81,21 @@
         width="50%"
         center
       >
-        <span slot="title" class="dialog-title">Pengumuman Peningkatan Kemudahan Pengguna Fitur Verval Sapawarga</span>
-        <p class="dialog-content">Tanggal Pengumuman: 5 Mei 2020</p>
-        <p class="dialog-content">Assalamualaikum wr. wb.</p>
-        <p class="dialog-content">Sampurasun, Pemerintah Desa/Kelurahan, Ketua RW, PLD, PSM, dan Karang Taruna!</p>
-        <p class="dialog-content">Terima kasih telah berpartisipasi dalam proses verifikasi dan validasi (verval) data penerima bansos melalui aplikasi Sapawarga pada PERIODE 1. Saat ini, proses pengalokasian bantuan sosial sedang berjalan.</p>
-        <p class="dialog-content">Sehubungan dengan upaya meningkatkan kemudahan penggunaan aplikasi Sapawarga untuk RW dan Pemerintah Desa/Kelurahan (Pemdes/Kel), untuk sementara fitur Verval akan kami tutup selama 5 HARI yakni mulai hari Rabu (6 Mei 2020) pukul 12.00 hingga hari Senin (11 Mei 2020) pukul 12.00.
-        </p>
-        <p class="dialog-content">Untuk info lebih lengkap dapat dilihat pada informasi berikut:</p>
-        <p class="link"><a href="https://sapawarga.jabarprov.go.id/#/info-penting?id=91" style="color:blue">https://sapawarga.jabarprov.go.id/#/info-penting?id=91</a></p>
-        <p class="footer">Tim Sapawarga</p>
+        <span slot="title" class="dialog-title">Sampurasun Pemerintah Desa, Ketua RW, PLD, PSM, dan Karang Taruna!</span>
+        <p class="dialog-content space">Proses verifikasi dan validasi (verval) data penerima bansos via Sapawarga PERIODE 2 kembali dibuka mulai Senin, 11 Mei 2020!</p>
+        <p class="dialog-content space"><b>Berikut merupakan peningkatan fitur dan kemudahan yang dapat digunakan oleh pengguna pada proses verval bansos di periode kedua : </b></p>
+        <p class="dialog-content space">1. Pemerintah Desa atau Ketua RW dapat mengupdate/merubah status calon penerima bansos (dari ditolak menjadi terverifikasi, atau sebaliknya).</p>
+        <p class="dialog-content space">2. Pembenahan terhadap permasalahan terkait KTP ganda dan data tersasar.</p>
+        <p class="dialog-content space">3. Pengakomodasian pendaftaran penerima bansos non-KTP.</p>
+        <p class="dialog-content space">4. Penyelesaian permasalahan error 422,413,504, dan 408 pada Admin Sapawarga tingkat desa dan aplikasi Sapawarga Ketua RW.</p>
+        <p class="dialog-content space">Demikian pengumuman kami, dimohon agar maklum.</p>
+        <p class="dialog-content space">Mari dukung proses verifikasi dan validasi data penerima bantuan sosial melalui Sapawarga untuk pelayanan dan proses pemberian bansos yang lebih baik.</p>
+        <p class="dialog-content space">Hormat kami,</p>
+        <p class="dialog-content"><b>Tim Sapawarga,</b></p>
+        <p class="dialog-content" style="margin-top:-12px">Pemerintah Provinsi Jawa Barat</p>
+        <p class="dialog-content">Hotline (Hanya Whatsapp)</p>
+        <p class="dialog-content" style="margin-top:-12px">+62 812-2008-2668</p>
+        <p class="dialog-content link"><a href="https://s.id/HotlineSW" style="color:blue">https://s.id/HotlineSW</a></p>
         <span slot="footer" class="dialog-footer">
           <el-button type="success" @click="dialogVisible = false">Tutup</el-button>
         </span>
@@ -159,7 +168,6 @@ export default {
 
   methods: {
     accessBlock(value) {
-      // this.dialogVisible = true
       this.$router.push('/beneficiaries/' + value)
     },
     // get summary statistics
@@ -229,15 +237,37 @@ export default {
     font-size: 22px;
     float: left;
     padding: 10px;
+    margin-bottom: 20px;
   }
   .dialog-content {
+    padding: 0 10px;
     font-size: 16px;
-    line-height: 25px;
-  }
-  .link {
-    color: blue !important;
+    .space {
+      line-height: 25px;
+    }
+    .link {
+      color: blue !important;
+    }
   }
   .footer {
     padding-top: 10px;
+  }
+  .warn-content-warning {
+    background: #2b823d;
+    border-radius: 5px;
+    padding: 1.5rem;
+    word-spacing: .05rem;
+    color: white;
+    margin-bottom: 25px;
+
+    .title {
+      font-weight: 600;
+    }
+
+    .link {
+      text-decoration: underline;
+      cursor: pointer;
+      color: white !important;
+    }
   }
 </style>
