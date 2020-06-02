44eb9e4ab751a1273baa57ff05a052f518400cc5
muhammad yusuf <muhammadd.yzf@gmail.com>
2020-06-02 20:25:21 +0700

    hot fix upload data bnba

--- backend/src/views/bansos/download.vue
+++ backend/src/views/bansos/download.vue
@@ -2,7 +2,10 @@
   <div class="app-container">
     <el-row v-if="user.roles_active.id === 'staffKabkota'">
       <el-button type="text" @click="exportAll">
-        <img src="https://firebasestorage.googleapis.com/v0/b/sapawarga-app.appspot.com/o/admin-banner-01.png?alt=media&token=2c606be5-7378-40ca-b147-f19b8b8539ea" width="100%">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/sapawarga-app.appspot.com/o/admin-banner-01.png?alt=media&token=2c606be5-7378-40ca-b147-f19b8b8539ea"
+          width="100%"
+        >
       </el-button>
     </el-row>
 
@@ -14,7 +17,11 @@
           <el-row :gutter="10">
             <el-col :xs="{span:24, tag:'mb-10'}" :sm="24" :md="24">
               <el-form-item style="margin-bottom: 0">
-                <el-input v-model="listQuery.search" :placeholder="`${$t('label.beneficiaries-download-search-area')} ${areaLabelByFilter}`" clearable />
+                <el-input
+                  v-model="listQuery.search"
+                  :placeholder="`${$t('label.beneficiaries-download-search-area')} ${areaLabelByFilter}`"
+                  clearable
+                />
               </el-form-item>
             </el-col>
           </el-row>
@@ -27,52 +34,102 @@
               <el-col :xs="24" :md="21">
                 <span>{{ `${ $t('label.beneficiaries-download-list') } ${ areaLabelByFilter }` }}</span>
               </el-col>
-              <el-col :xs="24" :md="3">
-                <el-button type="success" plain icon="el-icon-download" @click="exportBySubdistrict">{{ $t('crud.export') }}</el-button>
+              <el-col v-if="user.roles_active.id === 'staffKabkota'" :xs="24" :md="3">
+                <el-button
+                  type="success"
+                  plain
+                  icon="el-icon-download"
+                  @click="exportBySubdistrict"
+                >{{ $t('crud.export') }}</el-button>
               </el-col>
             </el-row>
           </div>
           <div class="text item">
-            <el-table v-loading="listLoading" :data="sortedList.filter(data => !listQuery.search || data.name.toLowerCase().includes(listQuery.search.toLowerCase()))" border stripe highlight-current-row style="width: 100%" @sort-change="changeSort" @selection-change="selectChange">
+            <el-table
+              v-loading="listLoading"
+              :data="sortedList.filter(data => !listQuery.search || data.name.toLowerCase().includes(listQuery.search.toLowerCase()))"
+              border
+              stripe
+              highlight-current-row
+              style="width: 100%"
+              @sort-change="changeSort"
+              @selection-change="selectChange"
+            >
               <el-table-column type="selection" align="center" width="55" />
-              <el-table-column type="index" width="50" align="center" :index="getTableRowNumbering" />
-              <el-table-column prop="name" sortable="custom" :label="areaLabelByFilter" min-width="200px">
+              <el-table-column
+                type="index"
+                width="50"
+                align="center"
+                :index="getTableRowNumbering"
+              />
+              <el-table-column
+                prop="name"
+                sortable="custom"
+                :label="areaLabelByFilter"
+                min-width="200px"
+              >
                 <template slot-scope="{row}">
                   <span>{{ row.name }}</span>
                 </template>
               </el-table-column>
-              <el-table-column prop="data.approved" align="right" sortable="custom" :label="$t('label.beneficiaries-verified-rw')" min-width="180px">
+              <el-table-column
+                prop="data.approved"
+                align="right"
+                sortable="custom"
+                :label="$t('label.beneficiaries-verified-rw')"
+                min-width="180px"
+              >
                 <template slot-scope="{row}">
-                  <span v-if="row.data.approved" style="float: left">
-                    ({{ formatNumber(percentage(row.data.approved, getTotalBenefeciaries(row.data))) }}%)
-                  </span>
+                  <span
+                    v-if="row.data.approved"
+                    style="float: left"
+                  >({{ formatNumber(percentage(row.data.approved, getTotalBenefeciaries(row.data))) }}%)</span>
                   {{ row.data.approved | currency }}
                 </template>
               </el-table-column>
-              <el-table-column prop="data.pending" align="right" sortable="custom" :label="$t('label.beneficiaries-unverified')" min-width="180px">
+              <el-table-column
+                prop="data.pending"
+                align="right"
+                sortable="custom"
+                :label="$t('label.beneficiaries-unverified')"
+                min-width="180px"
+              >
                 <template slot-scope="{row}">
-                  <span v-if="row.data.pending" style="float: left">
-                    ({{ formatNumber(percentage(row.data.pending, getTotalBenefeciaries(row.data))) }}%)
-                  </span>
+                  <span
+                    v-if="row.data.pending"
+                    style="float: left"
+                  >({{ formatNumber(percentage(row.data.pending, getTotalBenefeciaries(row.data))) }}%)</span>
                   {{ row.data.pending | currency }}
                 </template>
               </el-table-column>
-              <el-table-column prop="data.reject" align="right" sortable="custom" :label="$t('label.beneficiaries-reject')" min-width="180px">
+              <el-table-column
+                prop="data.reject"
+                align="right"
+                sortable="custom"
+                :label="$t('label.beneficiaries-reject')"
+                min-width="180px"
+              >
                 <template slot-scope="{row}">
-                  <span v-if="getReject(row.data)" style="float: left">
-                    ({{ formatNumber(percentage(getReject(row.data), getTotalBenefeciaries(row.data))) }}%)
-                  </span>
+                  <span
+                    v-if="getReject(row.data)"
+                    style="float: left"
+                  >({{ formatNumber(percentage(getReject(row.data), getTotalBenefeciaries(row.data))) }}%)</span>
                   {{ getReject(row.data) | currency }}
                 </template>
               </el-table-column>
-              <el-table-column prop="data_baru.total" align="right" sortable="custom" :label="$t('label.beneficiaries-newdata')" min-width="180px">
+              <el-table-column
+                prop="data_baru.total"
+                align="right"
+                sortable="custom"
+                :label="$t('label.beneficiaries-newdata')"
+                min-width="180px"
+              >
                 <template slot-scope="{row}">
-                  <span v-if="row.data_baru && row.data_baru.total" style="float: left">
-                    ({{ formatNumber(percentage(row.data_baru.total, getTotalBenefeciaries(row.data))) }}%)
-                  </span>
-                  <span v-if="row.data_baru">
-                    {{ row.data_baru.total | currency }}
-                  </span>
+                  <span
+                    v-if="row.data_baru && row.data_baru.total"
+                    style="float: left"
+                  >({{ formatNumber(percentage(row.data_baru.total, getTotalBenefeciaries(row.data))) }}%)</span>
+                  <span v-if="row.data_baru">{{ row.data_baru.total | currency }}</span>
                 </template>
               </el-table-column>
             </el-table>
@@ -162,54 +219,96 @@ export default {
       const percentage = this.percentage
       function compare(a, b) {
         if (prop === 'data.approved_kabkota') {
-          if (percentage(a.data.approved_kabkota, getTotalBenefeciaries(a.data)) < percentage(b.data.approved_kabkota, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a.data.approved_kabkota, getTotalBenefeciaries(a.data)) <
+            percentage(b.data.approved_kabkota, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? -1 : 1
           }
-          if (percentage(a.data.approved_kabkota, getTotalBenefeciaries(a.data)) > percentage(b.data.approved_kabkota, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a.data.approved_kabkota, getTotalBenefeciaries(a.data)) >
+            percentage(b.data.approved_kabkota, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? 1 : -1
           }
         } else if (prop === 'data.approved_kec') {
-          if (percentage(a.data.approved_kec, getTotalBenefeciaries(a.data)) < percentage(b.data.approved_kec, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a.data.approved_kec, getTotalBenefeciaries(a.data)) <
+            percentage(b.data.approved_kec, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? -1 : 1
           }
-          if (percentage(a.data.approved_kec, getTotalBenefeciaries(a.data)) > percentage(b.data.approved_kec, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a.data.approved_kec, getTotalBenefeciaries(a.data)) >
+            percentage(b.data.approved_kec, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? 1 : -1
           }
         } else if (prop === 'data.approved_kel') {
-          if (percentage(a.data.approved_kel, getTotalBenefeciaries(a.data)) < percentage(b.data.approved_kel, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a.data.approved_kel, getTotalBenefeciaries(a.data)) <
+            percentage(b.data.approved_kel, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? -1 : 1
           }
-          if (percentage(a.data.approved_kel, getTotalBenefeciaries(a.data)) > percentage(b.data.approved_kel, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a.data.approved_kel, getTotalBenefeciaries(a.data)) >
+            percentage(b.data.approved_kel, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? 1 : -1
           }
         } else if (prop === 'data.approved') {
-          if (percentage(a.data.approved, getTotalBenefeciaries(a.data)) < percentage(b.data.approved, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a.data.approved, getTotalBenefeciaries(a.data)) <
+            percentage(b.data.approved, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? -1 : 1
           }
-          if (percentage(a.data.approved, getTotalBenefeciaries(a.data)) > percentage(b.data.approved, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a.data.approved, getTotalBenefeciaries(a.data)) >
+            percentage(b.data.approved, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? 1 : -1
           }
         } else if (prop === 'data_baru.total') {
           var a_data_baru_total = a.data_baru ? a.data_baru.total || 0 : 0
           var b_data_baru_total = b.data_baru ? b.data_baru.total || 0 : 0
-          if (percentage(a_data_baru_total, getTotalBenefeciaries(a.data)) < percentage(b_data_baru_total, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a_data_baru_total, getTotalBenefeciaries(a.data)) <
+            percentage(b_data_baru_total, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? -1 : 1
           }
-          if (percentage(a_data_baru_total, getTotalBenefeciaries(a.data)) > percentage(b_data_baru_total, getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(a_data_baru_total, getTotalBenefeciaries(a.data)) >
+            percentage(b_data_baru_total, getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? 1 : -1
           }
         } else if (prop === 'data.pending') {
-          if (percentage(getPending(a.data), getTotalBenefeciaries(a.data)) < percentage(getPending(b.data), getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(getPending(a.data), getTotalBenefeciaries(a.data)) <
+            percentage(getPending(b.data), getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? -1 : 1
           }
-          if (percentage(getPending(a.data), getTotalBenefeciaries(a.data)) > percentage(getPending(b.data), getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(getPending(a.data), getTotalBenefeciaries(a.data)) >
+            percentage(getPending(b.data), getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? 1 : -1
           }
         } else if (prop === 'data.reject') {
-          if (percentage(getReject(a.data), getTotalBenefeciaries(a.data)) < percentage(getReject(b.data), getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(getReject(a.data), getTotalBenefeciaries(a.data)) <
+            percentage(getReject(b.data), getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? -1 : 1
           }
-          if (percentage(getReject(a.data), getTotalBenefeciaries(a.data)) > percentage(getReject(b.data), getTotalBenefeciaries(b.data))) {
+          if (
+            percentage(getReject(a.data), getTotalBenefeciaries(a.data)) >
+            percentage(getReject(b.data), getTotalBenefeciaries(b.data))
+          ) {
             return order === 'ascending' ? 1 : -1
           }
         } else {
@@ -223,7 +322,7 @@ export default {
         return 0
       }
       if (this.list) {
-        return this.list.map((x) => x).sort(compare)
+        return this.list.map(x => x).sort(compare)
       } else {
         return []
       }
@@ -236,10 +335,13 @@ export default {
   methods: {
     formatNumber,
     percentage(val, denom) {
-      return denom ? val / denom * 100 : 0
+      return denom ? (val / denom) * 100 : 0
     },
     resetParams() {
-      if (this.user.roles_active.id === 'staffProv' || this.user.roles_active.id === 'admin') {
+      if (
+        this.user.roles_active.id === 'staffProv' ||
+        this.user.roles_active.id === 'admin'
+      ) {
         this.filter.type = 'provinsi'
         this.filter.code_bps = '32'
       }
@@ -263,10 +365,12 @@ export default {
       this.listLoading = false
     },
     getTotalBenefeciaries(data) {
-      return this.getApproved(data) + this.getPending(data) + this.getReject(data)
+      return (
+        this.getApproved(data) + this.getPending(data) + this.getReject(data)
+      )
     },
     getTableRowNumbering(index) {
-      return (index + 1)
+      return index + 1
     },
     changeSort(e) {
       this.sort_prop = e.prop
@@ -279,13 +383,23 @@ export default {
       this.listQuery.kode_kec = id
     },
     getApproved(data) {
-      return data.approved_kabkota + data.approved_kec + data.approved_kel + data.approved
+      return (
+        data.approved_kabkota +
+        data.approved_kec +
+        data.approved_kel +
+        data.approved
+      )
     },
     getPending(data) {
       return data.pending
     },
     getReject(data) {
-      return data.rejected_kabkota + data.rejected_kec + data.rejected_kel + data.rejected
+      return (
+        data.rejected_kabkota +
+        data.rejected_kec +
+        data.rejected_kel +
+        data.rejected
+      )
     },
     selectChange(data) {
       const result = []
@@ -297,7 +411,9 @@ export default {
     async exportData(params) {
       const response = await exportExcel(params)
       const dateNow = Date.now()
-      const fileName = `${this.$t('label.beneficiaries-download-bnba-document')} - ${moment(dateNow).format('D MMMM YYYY H:mm:ss')}.xlsx`
+      const fileName = `${this.$t(
+        'label.beneficiaries-download-bnba-document'
+      )} - ${moment(dateNow).format('D MMMM YYYY H:mm:ss')}.xlsx`
       FileSaver.saveAs(response, fileName)
     },
     exportBySubdistrict() {
