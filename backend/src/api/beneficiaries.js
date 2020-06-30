7fc46ad04568764df0bb77c4a27e45f944d2081b
maruf <maruf.harsono@gmail.com>
2020-06-30 11:42:54 +0700

    Beneficiaries: added new endpoint

--- backend/src/api/beneficiaries.js
+++ backend/src/api/beneficiaries.js
@@ -209,3 +209,18 @@ export function fetchCurrentTahap(query) {
   })
 }
 
+export function fetchDownloadStatusBnba(query) {
+  return request({
+    url: '/beneficiaries-bnba/download-status',
+    method: 'get',
+    params: query
+  })
+}
+
+export function fetchDownloadStatus(query) {
+  return request({
+    url: '/beneficiaries/download-status',
+    method: 'get',
+    params: query
+  })
+}
