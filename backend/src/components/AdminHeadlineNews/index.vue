ac50a65f99b63b84eef52ff82ba454839aba4e8c
aldi <aldirohman@live.com>
2020-07-21 09:15:48 +0700

    add component banners

--- backend/src/components/AdminHeadlineNews/index.vue
+++ backend/src/components/AdminHeadlineNews/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <el-row v-if="user.roles_active.id === 'staffKabkota'">
-      <a :href="link" target="_blank">
+      <el-carousel indicator-position="outside" :autoplay="true" trigger="click">
+        <el-carousel-item v-for="(item, index) in bannerData" :key="index">
+          <img :src="item" class="multiple-images">
+        </el-carousel-item>
+      </el-carousel>
+      <!-- <a :href="link" target="_blank">
         <img src="https://firebasestorage.googleapis.com/v0/b/sapawarga-app.appspot.com/o/admin-banner-01.png?alt=media&token=2c606be5-7378-40ca-b147-f19b8b8539ea" width="100%">
-      </a>
+      </a> -->
     </el-row>
   </div>
 </template>
@@ -11,11 +16,16 @@
 <script>
 import { mapGetters } from 'vuex'
 import { getLink } from '@/api/temporary'
+import { PREFIX_FIREBASE_STORAGE } from '@/utils/constantVariable'
 
 export default {
   data() {
     return {
-      link: null
+      link: null,
+      bannerData: [
+        `${PREFIX_FIREBASE_STORAGE}banners%2Fbanner-1.png?alt=media&token=f8575a3e-8f16-491e-8d5b-ca78ca715c31`,
+        `${PREFIX_FIREBASE_STORAGE}banners%2Fbanner-2.png?alt=media&token=d9472add-eafd-4c6d-87ee-c61c8f701d7d`
+      ]
     }
   },
 
@@ -47,5 +57,12 @@ export default {
 }
 #headline-news .el-card__body {
   background-color: #FDF6EC;
+}
+.multiple-images {
+  width: 100%;
+  height: 100%;
+  display: block;
+  border-radius: 7px;
+  margin: auto;
 }
 </style>
