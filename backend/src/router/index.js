35ae6e887e39b5cd94d33e33a2033ca4e8bb2759
aldi <aldirohman@live.com>
2020-08-13 14:27:41 +0700

    comment whitelist 11 kabkota access download upload BNBA

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -30,7 +30,7 @@ import surveyLeaderMenuRouter from './modules/LeaderMenu/survey-leader-menu'
 import newsLeaderMenuRouter from './modules/LeaderMenu/news-leader-menu'
 import newsHoaxLeaderMenuRouter from './modules/LeaderMenu/newsHoax-leader-menu'
 import qnaLeaderMenuRouter from './modules/LeaderMenu/qna-leader-menu'
-import { KAB_KOTA_ACCESS_BNBA } from '@/utils/constantVariable'
+// import { KAB_KOTA_ACCESS_BNBA } from '@/utils/constantVariable'
 
 /** note: sub-menu only appear when children.length>=1
  *  detail see  https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
@@ -207,7 +207,7 @@ export const asyncRoutes = [
     redirect: 'noredirect',
     meta: {
       title: 'download-upload-menu',
-      roles: KAB_KOTA_ACCESS_BNBA,
+      roles: ['admin', 'staffKabkota'],
       icon: 'example'
     },
     active: false,
@@ -219,7 +219,7 @@ export const asyncRoutes = [
         hidden: false,
         meta: {
           title: 'download-upload-menu',
-          roles: KAB_KOTA_ACCESS_BNBA
+          roles: ['staffKabkota']
         }
       }
     ]
