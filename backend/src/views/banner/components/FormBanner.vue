6e0e72ee438d4ec3a057bbfeead40602ed1c5799
Imam Muslim <imammuslim060892@gmail.com>
2021-06-10 07:53:41 +0000

    Merge branch 'feature/Feature-list-bnba' into 'develop'

--- backend/src/views/banner/components/FormBanner.vue
+++ backend/src/views/banner/components/FormBanner.vue
@@ -5,7 +5,7 @@
         <AttachmentPhotoUpload
           type="banner_photo"
           :initial-url="banner.image_path_url"
-          :list-information="[this.$t('label.maximum-dimension-image'), this.$t('label.maximum-size-image')]"
+          :list-information="[$t('label.maximum-dimension-image'), $t('label.maximum-size-image')]"
           style="margin-bottom: 25px"
           @onUpload="photoUploaded"
         />
